2e4acfd35d8b33af6ad54602808dc2d48eed5bcb
박유빈 <qkrdbqls1001@naver.com>
2024-09-02 18:00:18 +0900

    tmp: 로컬 Pin 기반 파일 업로드-다운로드 연동 중 임시 저장

--- backend/ipsf-server/src/router/ipfs.js
+++ backend/ipsf-server/src/router/ipfs.js
@@ -1,14 +1,36 @@
 import express from "express";
 
 import { upload } from '../util/file-process.js'
-import { uploadAndPin } from "../service/pinata.js";
+import { uploadAndPin, getPinnedData, testFunction } from "../service/pinata.js";
+import { calculateTotalPinnedSize, pinToLocal, isPinnedInLocal, getFileMimetype } from '../service/local-ipfs.js';
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    console.log('Total size:', await calculateTotalPinnedSize());
+    res.send();
+})
+
 router.post('/', upload.single('file'), async (req, res) => {
     const { file } = req;
-    await uploadAndPin(file);
-    res.send('Done!');
+    const result = await uploadAndPin(file);
+    pinToLocal(result.IpfsHash);
+    res.send(result);
+})
+
+router.get('/:hash', async (req, res) => {
+    const { hash } = req.params;
+    
+    const pinnedData = await getPinnedData(hash);
+    console.log(pinnedData);
+    
+    const mimeType = await getFileMimetype(pinnedData.data);
+    res.setHeader('Content-Type', mimeType || 'application/octet-stream');
+    res.setHeader('Content-Disposition', `attachment; filename="${hash}"`);
+
+    pinnedData.data.arrayBuffer().then((buf) => {
+        res.send(Buffer.from(buf))
+    });
 })
 
 export default router;
