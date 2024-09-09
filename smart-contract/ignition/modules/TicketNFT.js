819891275bc7eb32a6401d8b42e3c0a2a872ee4c
kkh <kkho9654@naver.com>
2024-09-10 08:58:42 +0900

    Feat: smart contract 배포 - 테스트 코드 - scrpits - 배포 코드 - iginition/modules - smart contract 코드 - contracts

--- smart-contract/ignition/modules/TicketNFT.js
+++ smart-contract/ignition/modules/TicketNFT.js
@@ -3,7 +3,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 module.exports = buildModule("TicketNFT", (m) => {
 
   // TicketNFT 배포
-  const ticketNFT = m.contract("TicketNFT", ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"]);
+  const ticketNFT = m.contract("TicketNFT", ["0x6009560432b36eFC5E0F649B62a7783e4ecD0B03"]);
 
   return { ticketNFT };// , ticketNFT, concertManager };
 });
--- smart-contract/scripts/melodyToken.js
+++ smart-contract/scripts/melodyToken.js
@@ -1,23 +1,26 @@
 const { ethers } = require("hardhat");
 async function main() {
-    const melodyTokenAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"; // MelodyToken 배포 주소
-    const MelodyToken = await ethers.getContractFactory("MelodyToken");
+  const [owner, addr1, addr2] = await ethers.getSigners();
+
+    const melodyTokenAddress = "0xdA46A09167EbcE0bc27847eaD2Ebc31cF060F71E"; // MelodyToken 배포 주소
+    const MelodyToken = (await ethers.getContractFactory("MelodyToken"));
+    // const melodyToken = await MelodyToken.connect(owner).deploy(100000, owner.address); // initialSupply: 1000, 소유자: owner
+    // (await melodyToken.waitForDeployment().then((x)=>{console.log(x.getAddress())}));
     const melodyToken = await MelodyToken.attach(melodyTokenAddress);
   
-    const [owner, addr1, addr2, addr3] = await ethers.getSigners();
   
     // Owner가 addr1에게 1000 MLDY 발행
-    const mintTx = await melodyToken.mint(addr1.address, ethers.parseEther("1000"));
+    const mintTx = await melodyToken.connect(owner).mint(addr1.address, 1000);
     await mintTx.wait();
   
     // addr1이 addr2에게 500 MLDY 전송
-    const transferTx = await melodyToken.connect(addr1).transfer(addr3.address, ethers.parseEther("500"));
+    const transferTx = await melodyToken.connect(addr1).transfer(addr2.address, 500);
     await transferTx.wait();
   
     // 잔액 확인
-    console.log("Owner balance:", ethers.formatEther(await melodyToken.balanceOf(owner.address)));
-    console.log("Addr1 balance:", ethers.formatEther(await melodyToken.balanceOf(addr1.address)));
-    console.log("Addr3 balance:", ethers.formatEther(await melodyToken.balanceOf(addr3.address)));
+    console.log("Owner balance:", await melodyToken.balanceOf(owner.address));
+    console.log("Addr1 balance:", await melodyToken.balanceOf(addr1.address));
+    console.log("Addr3 balance:", await melodyToken.balanceOf(addr2.address));
   }
   
   main().catch((error) => {
