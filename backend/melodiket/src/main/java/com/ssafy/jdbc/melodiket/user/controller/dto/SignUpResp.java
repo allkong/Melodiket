d914a6d880ca12296c57fb9672a242beaeabe01a
박세빈 <psv980817@naver.com>
2024-09-10 17:42:36 +0900

    SignUp 관련 DTO 구현 (Valid 구현)

---
+++ backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/user/controller/dto/SignUpResp.java
@@ -0,0 +1,15 @@
+package com.ssafy.jdbc.melodiket.user.controller.dto;
+
+import com.ssafy.jdbc.melodiket.user.entity.Role;
+
+import java.util.UUID;
+
+public record SignUpResp(
+        Long id,
+        UUID uuid,
+        String loginId,
+        String nickname,
+        Role role,
+        String description
+) {
+}
