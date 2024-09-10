7058e1b94957a652eed308f2f5cbc847cf0caa2c
박세빈 <psv980817@naver.com>
2024-09-10 17:45:41 +0900

    AppUserRepository 구현

---
+++ backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/user/repository/AppUserRepository.java
@@ -0,0 +1,18 @@
+package com.ssafy.jdbc.melodiket.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.ssafy.jdbc.melodiket.user.entity.AppUser;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findByUuid(UUID uuid);
+    Optional<AppUser> findByLoginId(String loginId);
+    boolean existsByLoginId(String loginId);
+    boolean existsByNickname(String nickname);
+}
+
