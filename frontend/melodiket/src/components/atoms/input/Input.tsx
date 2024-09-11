211cca22b69d8b33e914177f7653e54b74133a3d
강혁준 <domohit5124@naver.com>
2024-09-11 12:36:43 +0900

    feat: Input 컴포넌트 구현

---
+++ frontend/melodiket/src/components/atoms/input/Input.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import {
+  ChangeEvent,
+  ComponentProps,
+  ForwardedRef,
+  forwardRef,
+  KeyboardEvent,
+  useState,
+} from 'react';
+
+interface InputProps {
+  value?: string;
+  onChange?: (value: string) => void;
+  onClickEnter?: () => void;
+  placeholder?: string;
+  type?: Extract<ComponentProps<'input'>['type'], 'text' | 'password'>;
+}
+
+const Input = forwardRef(
+  (
+    {
+      value: controlledValue,
+      onChange,
+      onClickEnter,
+      placeholder,
+      type,
+    }: InputProps,
+    ref?: ForwardedRef<HTMLInputElement>
+  ) => {
+    const isControlled = !controlledValue;
+
+    const [value, setValue] = useState('');
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      const eventValue = e.target.value;
+      if (!isControlled) {
+        setValue(eventValue);
+      }
+
+      onChange?.(eventValue);
+    };
+
+    const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter') {
+        onClickEnter?.();
+      }
+    };
+
+    return (
+      <input
+        ref={ref}
+        className="w-full h-12 px-5 text-base border outline-none rounded-2xl"
+        type={type}
+        placeholder={placeholder}
+        value={isControlled ? controlledValue : value}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+    );
+  }
+);
+
+Input.displayName = 'Input';
+
+export default Input;
