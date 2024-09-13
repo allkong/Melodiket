21eb1b7edbd8ab108cb27050062c4a14e3c55575
Dabin Jeong <allempti@gmail.com>
2024-09-13 23:10:30 +0900

    feat: ControlsBar에서 선택 관리

--- frontend/melodiket/src/components/organisms/controls/ControlsBar.tsx
+++ frontend/melodiket/src/components/organisms/controls/ControlsBar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState } from 'react';
 
 import OptionButton from '@/components/atoms/button/OptionButton';
@@ -6,13 +8,22 @@ import SelectButton from '@/components/atoms/button/SelectButton';
 const ControlsBar = () => {
   const [sortOption, setSortOption] = useState<string | null>('가나다순');
   const [isFilter, setIsFilter] = useState(false);
+  const [selectedDropdownOption, setSelectedDropdownOption] = useState<
+    string | null
+  >(null);
 
-  const handleOptionClick = (option: string) => {
+  const handleOptionSelect = (option: string) => {
     setSortOption(option);
+    setSelectedDropdownOption(null);
   };
 
   const handleFilterClick = () => {
     setIsFilter((prev) => !prev);
+  };
+
+  const handleSelectChange = (option: string) => {
+    setSortOption(option);
+    setSelectedDropdownOption(option);
   };
 
   return (
@@ -20,19 +31,24 @@ const ControlsBar = () => {
       <OptionButton
         label="가나다순"
         isSelected={sortOption === '가나다순'}
-        onClick={() => handleOptionClick('가나다순')}
+        onClick={() => handleOptionSelect('가나다순')}
       />
       <OptionButton
         label="인기순"
         isSelected={sortOption === '인기순'}
-        onClick={() => handleOptionClick('인기순')}
+        onClick={() => handleOptionSelect('인기순')}
       />
       <OptionButton
         label="예매 중"
         isSelected={isFilter}
         onClick={handleFilterClick}
       />
-      <SelectButton options={['등록순', '최신순']} />
+      <SelectButton
+        options={['등록순', '최신순']}
+        selectedOption={selectedDropdownOption}
+        isSelected={sortOption === '등록순' || sortOption === '최신순'}
+        onSelect={handleSelectChange}
+      />
     </div>
   );
 };
