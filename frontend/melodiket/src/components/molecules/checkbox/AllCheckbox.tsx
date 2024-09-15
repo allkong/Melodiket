64960abc6fa0bc440845736ad110ff976517c3f1
강혁준 <domohit5124@naver.com>
2024-09-15 23:10:01 +0900

    feat: AllCheckbox 구현

---
+++ frontend/melodiket/src/components/molecules/checkbox/AllCheckbox.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import Checkbox from '@/components/atoms/checkbox/Checkbox';
+
+interface AllCheckboxProps {
+  label?: string;
+  isChecked?: boolean;
+  onChange?: (value: boolean) => void;
+}
+
+const AllCheckbox = ({ label, isChecked, onChange }: AllCheckboxProps) => {
+  const handleClick = () => {
+    onChange?.(!isChecked);
+  };
+
+  return (
+    <div onClick={handleClick} className="w-full flex items-center gap-2">
+      <Checkbox rounded isChecked={isChecked} onChange={onChange} />
+      <p className="flex-grow text-lg font-semibold cursor-pointer">{label}</p>
+    </div>
+  );
+};
+
+export default AllCheckbox;
