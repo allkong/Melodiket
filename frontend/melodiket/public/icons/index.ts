7f79ede0a69b2391695071f08168a149ede2b808
Dabin Jeong <allempti@gmail.com>
2024-09-19 05:42:27 +0900

    feat: MusicianItem 컴포넌트 구현 - Profile 컴포넌트 임시 구현 - FavoriteButton 컴포넌트 임시 구현

--- frontend/melodiket/public/icons/index.ts
+++ frontend/melodiket/public/icons/index.ts
@@ -2,5 +2,6 @@ import LogoText from './logo-text.svg';
 import MenuLine from './menu-line.svg';
 import SearchLine from './search-line.svg';
 import Arrow from './arrow-filled.svg';
+import Heart from './heart.svg';
 
-export { LogoText, MenuLine, SearchLine, Arrow };
+export { LogoText, MenuLine, SearchLine, Arrow, Heart };
