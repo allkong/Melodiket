6db2f50f3cda7a1fd7dc9beed7f6a5f0fd5324ce
박유빈 <qkrdbqls1001@naver.com>
2024-09-19 12:38:52 +0900

    feat: 솔리디티-자바 연동 준비

---
+++ backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/blockchain/config/BlockchainConfig.java
@@ -0,0 +1,42 @@
+package com.ssafy.jdbc.melodiket.blockchain.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+
+@Getter
+@Configuration
+public class BlockchainConfig {
+    private final String systemPrivateKey;
+    private final String systemWalletAddress;
+    private final String rpcUrl;
+    private final String melodiketContractAddress;
+    private final String melodyTokenContractAddress;
+
+    public BlockchainConfig(
+            @Value("${blockchain.system-private-key}") String systemPrivateKey,
+            @Value("${blockchain.system-wallet-address}") String systemWalletAddress,
+            @Value("${blockchain.rpc-url}") String rpcUrl,
+            @Value("${blockchain.contract-addresses.melodiket}") String melodiketContractAddress,
+            @Value("${blockchain.contract-addresses.melody-token}") String melodyTokenContractAddress
+    ) {
+        this.systemPrivateKey = systemPrivateKey;
+        this.systemWalletAddress = systemWalletAddress;
+        this.rpcUrl = rpcUrl;
+        this.melodiketContractAddress = melodiketContractAddress;
+        this.melodyTokenContractAddress = melodyTokenContractAddress;
+    }
+
+    @Bean
+    public Web3j web3j() {
+        return Web3j.build(new org.web3j.protocol.http.HttpService(rpcUrl));
+    }
+
+    @Bean
+    public Credentials credentials() {
+        return Credentials.create(systemPrivateKey);
+    }
+}
