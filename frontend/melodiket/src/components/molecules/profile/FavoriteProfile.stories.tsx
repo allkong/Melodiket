3d58a0892a89fdce159145560b1e3d07d437643c
allkong <allempti@gmail.com>
2024-09-19 16:08:39 +0900

    feat: FavoriteProfile 컴포넌트 구현

--- frontend/melodiket/src/components/molecules/profile/FavoriteProfile.stories.tsx
+++ frontend/melodiket/src/components/molecules/profile/FavoriteProfile.stories.tsx
@@ -11,5 +11,6 @@ type Story = StoryObj<typeof FavoriteProfile>;
 export const Default: Story = {
   args: {
     src: 'https://i.namu.wiki/i/PcRFH2yIjsqHjXPF3_MUtPT_KotbREADRzGZh9_XXktYOjHQxUm4fRR1xMz-_HsztNH78nTSUJ2ROzFNgYUkXUWXV-mJf-G1Ew4pGyu5ZXw_Of_wxUtEJwtbygJ5F_0pDSBTldSu6Zwd1D_-5SJLPQ.webp',
+    size: 'md',
   },
 };
--- frontend/melodiket/src/components/molecules/profile/FavoriteProfile.tsx
+++ frontend/melodiket/src/components/molecules/profile/FavoriteProfile.tsx
@@ -1,3 +1,4 @@
+import FavoriteButton from '@/components/atoms/button/FavoriteButton';
 import Profile from '@/components/atoms/profile/Profile';
 
 interface FavoriteProfile {
@@ -7,8 +8,11 @@ interface FavoriteProfile {
 
 const FavoriteProfile = ({ src, size }: FavoriteProfile) => {
   return (
-    <div>
+    <div className="relative w-fit">
       <Profile src={src} size={size} />
+      <div className="absolute bottom-0 right-0">
+        <FavoriteButton size={size} isOn />
+      </div>
     </div>
   );
 };
