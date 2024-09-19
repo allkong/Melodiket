9f1b5c09da179fb268ac91897958524abba8d071
윤병서 <bsyun0571@naver.com>
2024-09-19 15:58:40 +0900

    fix: 눌렀을 때, 체크박스 뜨도록 수정

--- frontend/melodiket/src/components/molecules/item/StageItem.tsx
+++ frontend/melodiket/src/components/molecules/item/StageItem.tsx
@@ -1,13 +1,28 @@
+import { useState } from 'react';
+import Checkbox from '@/components/atoms/checkbox/Checkbox';
+
 interface StageItemProps {
   title: string;
   content?: string;
 }
 
 const StageItem = ({ title, content }: StageItemProps) => {
+  const [isCheckedVisible, setIsCheckedVisible] = useState(false);
+
+  const handleClick = () => {
+    setIsCheckedVisible((prev) => !prev);
+  };
+
   return (
-    <div className="w-full p-4 bg-white">
-      <p className="text-black font-semibold ml-2">{title}</p>
-      {content && <p className="text-gray-300 ml-2">{content}</p>}
+    <div
+      onClick={handleClick}
+      className="w-full px-5 py-3 bg-white flex items-center justify-between"
+    >
+      <div>
+        <p className="text-black font-semibold">{title}</p>
+        {content && <p className="text-gray-300">{content}</p>}
+      </div>
+      {isCheckedVisible && <Checkbox isChecked={false} rounded={false} />}
     </div>
   );
 };
