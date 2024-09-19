a54333207e968c9469de46d6aabfa4a043c28794
allkong <allempti@gmail.com>
2024-09-19 15:12:34 +0900

    feat: Profile 컴포넌트 사이즈 커스텀

--- frontend/melodiket/src/components/atoms/profile/Profile.tsx
+++ frontend/melodiket/src/components/atoms/profile/Profile.tsx
@@ -1,13 +1,23 @@
+import clsx from 'clsx';
 import Image from 'next/image';
 
 interface ProfileProps {
   src: string;
+  size: 'sm' | 'md' | 'lg';
 }
 
-const Profile = ({ src }: ProfileProps) => {
+const Profile = ({ src, size }: ProfileProps) => {
   return (
-    // 크기는 나중에 다시 수정해야 함
-    <div className="p-0.5 rounded-full bg-gradient-to-b from-secondary to-primary w-[58px] h-[58px]">
+    <div
+      className={clsx(
+        'rounded-full bg-gradient-to-b from-secondary to-primary',
+        {
+          'p-0.5 w-14 h-14': size === 'sm',
+          'p-[0.2rem] w-20 h-20': size === 'md',
+          'p-1 w-28 h-28': size === 'lg',
+        }
+      )}
+    >
       <div className="relative flex items-center justify-center w-full h-full overflow-hidden rounded-full">
         <Image src={src} alt="profile" className="object-cover" fill />
       </div>
