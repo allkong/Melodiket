16a3adfdb44e4c11d851d465053cf5a2ee6391c7
kkh <kkho9654@naver.com>
2024-09-19 16:33:55 +0900

    Feat: melodiket entity

---
+++ backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/stage/entity/Stage.java
@@ -0,0 +1,48 @@
+package com.ssafy.jdbc.melodiket.stage.entity;
+
+import com.ssafy.jdbc.melodiket.concert.entity.Concert;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "stage")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Stage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private UUID uuid;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private Boolean isStanding;
+
+    private Long numOfRow;
+    private Long numOfCol;
+
+    @Column(nullable = false)
+    private Long capacity;
+
+    @OneToMany(mappedBy = "stage", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<StageAssignment> stageAssignments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "stage", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Concert> concerts = new ArrayList<>();
+
+}
---
+++ backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/user/entity/Musician.java
@@ -0,0 +1,29 @@
+package com.ssafy.jdbc.melodiket.user.entity;
+
+import com.ssafy.jdbc.melodiket.concert.entity.ConcertParticipantMusician;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "musician")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Musician extends AppUser {
+    @OneToOne
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private AppUser user;
+
+    private String description;
+
+    private String imageUrl;
+
+    @OneToMany(mappedBy = "musician", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ConcertParticipantMusician> concertParticipantMusicians = new ArrayList<>();
+
+}
