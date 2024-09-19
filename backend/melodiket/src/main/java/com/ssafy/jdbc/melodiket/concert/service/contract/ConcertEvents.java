a4e97a6bb87f279420552926b15bc503a984343e
박유빈 <qkrdbqls1001@naver.com>
2024-09-19 14:39:45 +0900

    feat: 콘서트 수락/거절 구현

--- backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/concert/service/contract/ConcertEvents.java
+++ backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/concert/service/contract/ConcertEvents.java
@@ -6,15 +6,52 @@ import org.web3j.abi.datatypes.Event;
 import org.web3j.abi.datatypes.generated.Uint256;
 
 import java.util.Arrays;
+import java.util.List;
 
 public abstract class ConcertEvents {
     public static final Event CONCERT_CREATED = new Event(
             "ConcertCreated",
             Arrays.asList(
                     new TypeReference<Uint256>() {
-                    },
+                    }, // concertId
                     new TypeReference<Address>() {
-                    }
+                    } // manager address
+            )
+    );
+
+    public static final Event ALL_MUSICIAN_AGREED = new Event(
+            "AllMusicianAgreed",
+            List.of(
+                    new TypeReference<Uint256>() {
+                    } // concertId
+            )
+    );
+
+    public static final Event MUSICIAN_AGREED = new Event(
+            "MusicianAgreed",
+            List.of(
+                    new TypeReference<Uint256>() {
+                    }, // concertId
+                    new TypeReference<Address>() {
+                    } // musician address
+            )
+    );
+
+    public static final Event MUSICIAN_DENIED = new Event(
+            "MusicianDenied",
+            List.of(
+                    new TypeReference<Uint256>() {
+                    }, // concertId
+                    new TypeReference<Address>() {
+                    } // musician address
+            )
+    );
+
+    public static final Event CONCERT_CANCELED = new Event(
+            "ConcertCanceled",
+            List.of(
+                    new TypeReference<Uint256>() {
+                    } // concertId
             )
     );
 }
