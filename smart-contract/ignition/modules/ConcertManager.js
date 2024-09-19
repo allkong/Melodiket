b540081c201c48958534102f47ca53b16667100e
박유빈 <qkrdbqls1001@naver.com>
2024-09-19 12:39:54 +0900

    feat: 공연 정보 전체 조회 컨트랙트 함수 작성

--- smart-contract/ignition/modules/ConcertManager.js
+++ smart-contract/ignition/modules/ConcertManager.js
@@ -7,7 +7,7 @@ module.exports = buildModule("ConcertManager", (m) => {
   const concertManager = m.contract("ConcertManager", 
     ["0xdA46A09167EbcE0bc27847eaD2Ebc31cF060F71E", //멜로디토큰
       "0xb01BCdD0Fa0Ca96Cdc4bf368dc09f1a0DB58dA09" // 티켓 NFT
-      ]);
+    ]);
 
   return { concertManager };// , ticketNFT, concertManager };
 });
