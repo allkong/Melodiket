58c6bddf7a0a3a4af03fcf7e257f4165a9c73805
kkh <kkho9654@naver.com>
2024-09-19 16:33:55 +0900

    Feat: melodiket entity

---
+++ backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/account/entity/Account.java
@@ -0,0 +1,35 @@
+package com.ssafy.jdbc.melodiket.account.entity;
+
+import com.ssafy.jdbc.melodiket.user.entity.AppUser;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="bank_account")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String ownerName;
+
+    @Column(unique = true, nullable = false)
+    private String number;
+
+    @Column(nullable = false)
+    private String bankName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private AppUser appUser;
+
+}
