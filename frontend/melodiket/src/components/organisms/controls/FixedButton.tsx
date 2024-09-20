33df1c161d8ffbeed787605f064c2a021aa9c830
allkong <allempti@gmail.com>
2024-09-20 17:47:00 +0900

    feat: FixedButton 컴포넌트 구현

---
+++ frontend/melodiket/src/components/organisms/controls/FixedButton.tsx
@@ -0,0 +1,25 @@
+import { ReactElement } from 'react';
+
+import LargeButton from '@/components/atoms/button/LargeButton';
+
+interface LargeButtonProps {
+  label: string;
+  onClick?: () => void;
+  disabled?: boolean;
+  icon?: ReactElement;
+}
+
+const FixedButton = ({ label, onClick, disabled, icon }: LargeButtonProps) => {
+  return (
+    <div className="fixed bottom-0 flex items-center justify-center w-full max-w-xl py-4 bg-white px-7">
+      <LargeButton
+        label={label}
+        onClick={onClick}
+        disabled={disabled}
+        icon={icon}
+      />
+    </div>
+  );
+};
+
+export default FixedButton;
