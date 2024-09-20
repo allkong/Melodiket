cf2c33f1fbfebaf821223ba47b34e841753fccc2
박세빈 <psv980817@naver.com>
2024-09-20 13:35:10 +0900

    feat: AppUser 가 SpringSecurity 의 권한 관리할 수 있게 변경

--- backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/user/entity/AppUser.java
+++ backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/user/entity/AppUser.java
@@ -7,8 +7,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,8 +22,8 @@ import java.util.UUID;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-public class AppUser {
+@Builder(toBuilder = true)
+public class AppUser implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -53,4 +57,33 @@ public class AppUser {
     @OneToMany(mappedBy = "appUser", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<AccountCertification> accountCertifications = new ArrayList<>();
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.getAuthority()));
+    }
+
+    @Override
+    public String getUsername() {
+        return loginId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; // 계정 만료 여부
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true; // 계정 잠금 여부
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; // 자격 증명 만료 여부
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true; // 계정 활성화 여부
+    }
 }
