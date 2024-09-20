b54ea3b52770f6a737e2f017428f5503d935ebfe
박세빈 <psv980817@naver.com>
2024-09-20 13:35:43 +0900

    feat: 본인정보 조회 및 업데이트 메소드 작성

--- backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/auth/service/AuthService.java
+++ backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/auth/service/AuthService.java
@@ -7,6 +7,10 @@ import com.ssafy.jdbc.melodiket.auth.controller.dto.SignUpReq;
 import com.ssafy.jdbc.melodiket.auth.controller.dto.SignUpResp;
 import com.ssafy.jdbc.melodiket.user.entity.AppUser;
 import com.ssafy.jdbc.melodiket.user.entity.Role;
+import com.ssafy.jdbc.melodiket.user.entity.controller.dto.UpdateUserReq;
+import com.ssafy.jdbc.melodiket.user.entity.controller.dto.UserProfileResp;
+
+import java.util.UUID;
 
 public interface AuthService {
     SignUpResp signUp(SignUpReq signUpReq, Role role);
@@ -14,8 +18,14 @@ public interface AuthService {
     void logout();
     AppUser findUserByUuid(String uuid) throws HttpResponseException;
     AppUser findUserByLoginId(String loginId) throws HttpResponseException;
-
+    // 아이디 중복 검사
     boolean checkLoginIdDuplication(String loginId);
+    // 닉네임 중복 검사
     boolean checkNicknameDuplication(String nickname);
+    // 본인 정보 조회
+    UserProfileResp getUserProfileByLoginId(String loginId) throws HttpResponseException;
+    // 유저 정보 업데이트
+    UserProfileResp updateUser(UUID uuid, UpdateUserReq updateUserReq);
+
 }
 
