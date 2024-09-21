dfb51e932e8689e24555356c024146b1748f2b08
Dabin Jeong <allempti@gmail.com>
2024-09-21 19:54:16 +0900

    feat: loginId, password 정보 받기

--- frontend/melodiket/src/app/auth/login/page.tsx
+++ frontend/melodiket/src/app/auth/login/page.tsx
@@ -1,9 +1,19 @@
+'use client';
+import { useState } from 'react';
+
+import { LogoText } from '@/public/icons';
 import Input from '@/components/atoms/input/Input';
 import TextBanner from '@/components/molecules/text/TextBanner';
 import FixedButton from '@/components/organisms/controls/FixedButton';
-import { LogoText } from '@/public/icons';
 
 const Page = () => {
+  const [loginId, setLoginId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // 로그인 실행
+  };
+
   return (
     <div className="flex flex-col items-center">
       <div className="my-7">
@@ -18,12 +28,21 @@ const Page = () => {
           />
         </div>
         <div className="space-y-3 w-full">
-          <Input placeholder="아이디" />
-          <Input placeholder="비밀번호" type="password" />
+          <Input value={loginId} onChange={setLoginId} placeholder="아이디" />
+          <Input
+            value={password}
+            onChange={setPassword}
+            placeholder="비밀번호"
+            type="password"
+          />
         </div>
         <p className="text-sm text-gray-400">ID/PW 찾기 | 회원가입</p>
       </div>
-      <FixedButton label="로그인" />
+      <FixedButton
+        label="로그인"
+        disabled={!loginId || !password}
+        onClick={handleLogin}
+      />
     </div>
   );
 };
