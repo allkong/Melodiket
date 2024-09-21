837613fd9b6ebd3ef8667b1cdefa435257cbea19
Dabin Jeong <allempti@gmail.com>
2024-09-21 19:25:22 +0900

    feat: 로그인 페이지 컴포넌트 위치 조정

--- frontend/melodiket/src/app/auth/login/page.tsx
+++ frontend/melodiket/src/app/auth/login/page.tsx
@@ -5,22 +5,24 @@ import { LogoText } from '@/public/icons';
 
 const Page = () => {
   return (
-    <div className="flex flex-col items-center px-6 space-y-10">
+    <div className="flex flex-col items-center">
       <div className="my-7">
         <LogoText />
       </div>
-      <div className="w-full">
-        <TextBanner
-          title={`안녕하세요.\n멜로디켓입니다.`}
-          description="회원 서비스를 이용하시려면 로그인 해주세요."
-          hasLogo
-        />
+      <div className="flex flex-col items-center px-6 space-y-10 mt-[8vh]">
+        <div className="w-full">
+          <TextBanner
+            title={`안녕하세요.\n멜로디켓입니다.`}
+            description="회원 서비스를 이용하시려면 로그인 해주세요."
+            hasLogo
+          />
+        </div>
+        <div className="space-y-3 w-full">
+          <Input placeholder="아이디" />
+          <Input placeholder="비밀번호" type="password" />
+        </div>
+        <p className="text-sm text-gray-400">ID/PW 찾기 | 회원가입</p>
       </div>
-      <div className="space-y-3">
-        <Input placeholder="아이디" />
-        <Input placeholder="비밀번호" type="password" />
-      </div>
-      <p className="text-sm text-gray-400">ID/PW 찾기 | 회원가입</p>
       <FixedButton label="로그인" />
     </div>
   );
