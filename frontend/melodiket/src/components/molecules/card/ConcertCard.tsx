f134b059dc13f87ad049a8fdbcb810b3efea5795
강혁준 <domohit5124@naver.com>
2024-09-23 14:45:18 +0900

    feat: 공연 페이지 ConcertCard 컴포넌트 구현

--- frontend/melodiket/src/components/molecules/card/ConcertCard.tsx
+++ frontend/melodiket/src/components/molecules/card/ConcertCard.tsx
@@ -2,32 +2,60 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import FavoriteButton from '@/components/atoms/button/FavoriteButton';
+import { CalendarFilled, Location } from '@/public/icons';
 
 interface ConcertCardProps {
-  href: string;
+  concertId: string;
+  href?: string;
+  musicians?: string[];
+  ticketingAt?: string;
+  stage?: string;
+  posterURL?: string;
+  isFavorite?: boolean;
+  onClickFavorite?: (id: string) => void;
 }
 
-const ConcertCard = ({}: ConcertCardProps) => {
+const ConcertCard = ({
+  concertId,
+  href,
+  musicians,
+  ticketingAt,
+  stage,
+  posterURL,
+  isFavorite,
+  onClickFavorite,
+}: ConcertCardProps) => {
   return (
-    <Link href="/">
-      <div className="w-44 p-1 bg-pink-400 flex flex-col">
+    <Link href={href ?? '/'}>
+      <div className="w-44 p-1 bg-white flex flex-col space-y-2">
         {/* 이미지 영역 */}
         <div className="relative w-[10.6rem] h-60">
           <Image
-            src=""
+            src={posterURL ?? ''}
             alt="concert card image"
             className="object-cover rounded-md"
             fill
           />
           <div className="absolute right-2 bottom-2">
-            <FavoriteButton />
+            <FavoriteButton
+              isOn={isFavorite}
+              onClick={() => onClickFavorite?.(concertId)}
+            />
           </div>
         </div>
         {/* 컨텐츠 영역 */}
         <div>
-          <p>킹누</p>
-          <p>2024.09.03</p>
-          <p>고척스카이돔</p>
+          <p className="text-xs font-medium truncate mb-1">
+            {musicians && musicians.join(' ')}
+          </p>
+          <div className="flex items-center gap-1 text-[8px] text-gray-500">
+            <CalendarFilled className="fill-current" />
+            {ticketingAt && <p>{ticketingAt}</p>}
+          </div>
+          <div className="flex items-center gap-1 text-[8px] text-gray-500 truncate">
+            <Location className="fill-current" />
+            {stage && <p>{stage}</p>}
+          </div>
         </div>
       </div>
     </Link>
