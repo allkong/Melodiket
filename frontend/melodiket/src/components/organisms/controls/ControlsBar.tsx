9f77362a0f61fd4f7fe12c6bf7652c78ac46f219
allkong <allempti@gmail.com>
2024-09-23 10:13:20 +0900

    feat: ControlsBar에서 SelectButton도 OptionButton으로 변경

--- frontend/melodiket/src/components/organisms/controls/ControlsBar.tsx
+++ frontend/melodiket/src/components/organisms/controls/ControlsBar.tsx
@@ -2,25 +2,16 @@
 
 import { useRouter, useSearchParams } from 'next/navigation';
 
-import {
-  SORT_OPTIONS,
-  FILTER_OPTIONS,
-  SELECT_OPTIONS,
-} from '@/constants/controlOptions';
+import { FILTER_OPTIONS, SORT_OPTIONS } from '@/constants/controlOptions';
 
 import OptionButton from '@/components/atoms/button/OptionButton';
-import SelectButton from '@/components/atoms/button/SelectButton';
 
 const ControlsBar = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const sortQuery = searchParams.get('sort') || 'alphabetical';
   const filterQuery = searchParams.get('filter') === 'true';
-  const selectOptions = Object.entries(SELECT_OPTIONS).map(([key, value]) => ({
-    value: key,
-    label: value,
-  }));
+  const sortQuery = searchParams.get('sort') || 'alphabetical';
 
   const updateQueryParams = (key: string, value: string | boolean) => {
     const params = new URLSearchParams(searchParams.toString());
@@ -28,39 +19,29 @@ const ControlsBar = () => {
     router.push(`?${params.toString()}`, { scroll: false });
   };
 
-  const handleOptionSelect = (option: string) => {
-    updateQueryParams('sort', option);
-  };
-
   const handleFilterClick = () => {
     updateQueryParams('filter', !filterQuery);
   };
 
+  const handleOptionClick = (option: string) => {
+    updateQueryParams('sort', option);
+  };
+
   return (
     <div className="flex flex-row gap-3 px-6 py-3 bg-white overflow-x-auto whitespace-nowrap scrollbar-hide">
-      {Object.keys(SORT_OPTIONS).map((option) => (
-        <OptionButton
-          key={option}
-          label={SORT_OPTIONS[option as keyof typeof SORT_OPTIONS]}
-          isSelected={sortQuery === option}
-          onClick={() => handleOptionSelect(option)}
-        />
-      ))}
       <OptionButton
         label={FILTER_OPTIONS.booking}
         isSelected={filterQuery}
         onClick={handleFilterClick}
       />
-      <SelectButton
-        options={selectOptions}
-        selectedOption={
-          selectOptions.some((option) => option.value === sortQuery)
-            ? sortQuery
-            : null
-        }
-        isSelected={selectOptions.some((option) => option.value === sortQuery)}
-        onSelect={handleOptionSelect}
-      />
+      {Object.keys(SORT_OPTIONS).map((option) => (
+        <OptionButton
+          key={option}
+          label={SORT_OPTIONS[option as keyof typeof SORT_OPTIONS]}
+          isSelected={sortQuery === option}
+          onClick={() => handleOptionClick(option)}
+        />
+      ))}
     </div>
   );
 };
