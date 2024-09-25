55d01fd6fe004dd0c0eedab576b745bcfefd3c6f
kkh <kkho9654@naver.com>
2024-09-25 10:52:20 +0900

    refactor: 유저관련 엔티티 user 패키지로 이동

--- backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/auth/repository/StageMangerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.jdbc.melodiket.auth.repository;
-
-import com.ssafy.jdbc.melodiket.user.entity.StageManagerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StageMangerRepository extends JpaRepository<StageManagerEntity, Long> {
-}
