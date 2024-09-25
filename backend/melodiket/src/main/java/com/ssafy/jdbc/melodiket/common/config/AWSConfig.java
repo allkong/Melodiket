531c123c08e1b71f80dfee28354fc22d2796b427
kkh <kkho9654@naver.com>
2024-09-25 16:34:12 +0900

    chore: aws 설정

--- backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/common/config/AWSConfig.java
+++ backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/common/config/AWSConfig.java
@@ -2,7 +2,7 @@ package com.ssafy.jdbc.melodiket.common.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AWSConfig {
+
     @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
@@ -19,10 +20,11 @@ public class AWSConfig {
     @Value("${cloud.aws.region.static}")
     private String region;
 
+
     @Bean
-    public AmazonS3Client amazonS3Client(){
+    public AmazonS3 amazonS3Client(){
         BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey,secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder
+        return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
                 .withRegion(region)
