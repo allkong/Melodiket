4b45c7c0c69b416066d34343293bc1bb821dd3ab
강혁준 <domohit5124@naver.com>
2024-09-26 17:44:17 +0900

    fix: ConcertCard 및 Carousel 스토리 목업 오류 수정

--- frontend/melodiket/src/components/molecules/carousel/Carousel.stories.tsx
+++ frontend/melodiket/src/components/molecules/carousel/Carousel.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Carousel from './Carousel';
-import { CAROUSEL_DATAS } from '@/constants/concertMocks';
 
 const meta: Meta<typeof Carousel> = {
   component: Carousel,
@@ -11,7 +10,26 @@ type Story = StoryObj<typeof Carousel>;
 
 export const Default: Story = {
   args: {
-    datas: CAROUSEL_DATAS,
+    datas: [
+      {
+        description: '누구일까?\n비밀의 콘서트',
+        index: 0,
+        location: '고척스카이돔',
+        title: '콘서트 제목',
+        ticketingAt: '2024.09.03',
+        posterURL:
+          'https://an2-img.amz.wtchn.net/image/v2/zhP6BveaBByYE74o9uUFYg.jpg?jwt=ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKdmNIUnpJanBiSW1SZk1USTRNSGczTWpCeE9EQWlYU3dpY0NJNklpOTJNaTl6ZEc5eVpTOXBiV0ZuWlM4eE5qZ3dOVEUxTmprM01EZzRNVFEyTVRBM0luMC5rRnByMkxWT2hNajZjclBXY1YwOXhWNXpVNGdMbW9pSnMwSm5XQWZVNHpR',
+      },
+      {
+        description: '눈이 반짝',
+        index: 1,
+        location: '양재',
+        title: '콘서트 제목',
+        ticketingAt: '2024.09.03',
+        posterURL:
+          'https://www.harpersbazaar.co.kr/resources_old/online/org_online_image/3574a333-f251-4e64-8141-8861b11a1e17.jpg',
+      },
+    ],
     size: 'md',
     rounded: false,
   },
