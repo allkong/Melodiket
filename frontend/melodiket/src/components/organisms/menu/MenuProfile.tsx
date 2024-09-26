1c36c1a739251ee3ec3e0762734a3c7f66905de8
강혁준 <domohit5124@naver.com>
2024-09-26 10:23:58 +0900

    feat: MenuProfile props 삭제 및 store 사용

--- frontend/melodiket/src/components/organisms/menu/MenuProfile.tsx
+++ frontend/melodiket/src/components/organisms/menu/MenuProfile.tsx
@@ -1,24 +1,34 @@
+'use client';
+
 import Profile from '@/components/atoms/profile/Profile';
 import ArrowButton from '@/components/atoms/button/ArrowButton';
 import OptionButton from '@/components/atoms/button/OptionButton';
+import useAuthStore from '@/store/authStore';
+import Link from 'next/link';
 
-interface MenuProfileProps {
-  nickname: string;
-  imageURL: string;
-  role: string;
-}
+const MenuProfile = () => {
+  const { user } = useAuthStore();
 
-const MenuProfile = ({ nickname, imageURL, role }: MenuProfileProps) => {
   return (
-    <div className="flex items-center gap-4 w-full h-[104px] px-5 py-4">
-      <Profile size="sm" src={imageURL} />
-      <div className="flex flex-col gap-3">
-        <div className="flex items-center gap-2">
-          <p>{nickname}</p>
-          <ArrowButton />
+    <div className="flex items-center gap-4 w-full h-[104px] px-5 py-4 text-gray-500">
+      <Profile size="sm" />
+      {user ? (
+        <div className="flex flex-col gap-3">
+          <div className="flex items-center gap-2">
+            <p>{user.nickname}</p>
+            <ArrowButton />
+          </div>
+          <OptionButton
+            label={user.role ?? 'Unknown User'}
+            isSelected={true}
+            onClick={() => {}}
+          />
         </div>
-        <OptionButton label={role} isSelected={true} onClick={() => {}} />
-      </div>
+      ) : (
+        <Link href="/auth/login">
+          <p>로그인 후 이용해주세요.</p>
+        </Link>
+      )}
     </div>
   );
 };
