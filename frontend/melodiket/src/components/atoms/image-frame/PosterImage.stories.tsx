0c52738721088a07cd736340b88903b7c6c119c6
allkong <allempti@gmail.com>
2024-09-27 17:54:56 +0900

    feat: PosterImage 컴포넌트 구현

--- frontend/melodiket/src/components/atoms/image-frame/PosterImage.stories.tsx
+++ frontend/melodiket/src/components/atoms/image-frame/PosterImage.stories.tsx
@@ -9,5 +9,8 @@ export default meta;
 type Story = StoryObj<typeof PosterImage>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    src: 'https://i.namu.wiki/i/kurz0p1Tm_YMWDDeOzBkBd2P0odt2vWapIJHvcVd1KWEhEoLYP-oDj4ZmZxi2dlVMKsT8NcWsMJOIWtiA5rSaHEm2RqzkfrlhC3HdWg0dNjYwGaom5oOwcHFiSPYexcvRXDtY0dnuJnr9JvWXP0Cng.webp',
+    size: 'sm',
+  },
 };
