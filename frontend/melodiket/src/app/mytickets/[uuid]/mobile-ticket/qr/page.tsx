c700d875e0a008ab6323f17eb1ff906ef2ded304
Dabin Jeong <allempti@gmail.com>
2024-09-29 18:34:48 +0900

    feat: qr코드 페이지 기본 배치

---
+++ frontend/melodiket/src/app/mytickets/[uuid]/mobile-ticket/qr/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import CloseButton from '@/components/atoms/button/CloseButton';
+import { LogoImage, LogoText } from '@/public/icons';
+import { useRouter } from 'next/navigation';
+
+const Page = () => {
+  const router = useRouter();
+
+  const handleClose = () => {
+    router.back();
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-xl font-medium">2024 IU HEREH WORLD TOUR CONCERT</h1>
+      <p className="text-lg text-gray-500">2024.09.08(일) 18:00</p>
+      <div>qr</div>
+      <div className="flex flex-col items-center">
+        <LogoImage className="mb-3 w-11 h-11" />
+        <LogoText />
+      </div>
+      <CloseButton onClick={handleClose} hasBorder />
+    </div>
+  );
+};
+
+export default Page;
