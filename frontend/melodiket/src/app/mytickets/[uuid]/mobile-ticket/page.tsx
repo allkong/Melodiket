e3eb6601cf6ce0849a33ec3e4d37b9cb481dad97
allkong <allempti@gmail.com>
2024-09-30 18:54:06 +0900

    feat: 모바일 티켓, qr코드 로딩 스피너 적용 - LoadingSpinner 컴포넌트 구현

--- frontend/melodiket/src/app/mytickets/[uuid]/mobile-ticket/page.tsx
+++ frontend/melodiket/src/app/mytickets/[uuid]/mobile-ticket/page.tsx
@@ -1,16 +1,25 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+
+import useTicketStore from '@/store/ticketStore';
+
 import SubHeader from '@/components/organisms/navigation/SubHeader';
 import TicketFrame from '@/components/atoms/image-frame/TicketFrame';
 import FixedButton from '@/components/organisms/controls/FixedButton';
 import { QrCode } from '@/public/icons';
-import { usePathname, useRouter } from 'next/navigation';
-import useTicketStore from '@/store/ticketStore';
+import LoadingSpinner from '@/components/atoms/feedback/LoadingSpinner';
 
 const Page = () => {
   const router = useRouter();
   const pathname = usePathname();
   const { ticketDetail } = useTicketStore();
+  const [isHydrated, setIsHydrated] = useState(false);
+
+  useEffect(() => {
+    setIsHydrated(true);
+  }, []);
 
   const handleClose = () => {
     router.back();
@@ -25,13 +34,19 @@ const Page = () => {
       <SubHeader title="모바일 티켓" onClose={handleClose} />
 
       <div className="flex flex-col items-center flex-grow px-10 pb-24 mb-2 justify-evenly overscroll-y-auto">
-        <h1 className="font-medium text-center text-gray-500 line-clamp-1">
-          {ticketDetail?.concertTitle}
-        </h1>
+        {isHydrated ? (
+          <>
+            <h1 className="font-medium text-center text-gray-500 line-clamp-1">
+              {ticketDetail?.concertTitle}
+            </h1>
 
-        <div className="flex-grow flex items-center justify-center max-h-[70vh]">
-          <TicketFrame src={ticketDetail?.posterCid || ''} />
-        </div>
+            <div className="flex-grow flex items-center justify-center max-h-[70vh]">
+              <TicketFrame src={ticketDetail?.posterCid || ''} />
+            </div>
+          </>
+        ) : (
+          <LoadingSpinner />
+        )}
       </div>
 
       <FixedButton
