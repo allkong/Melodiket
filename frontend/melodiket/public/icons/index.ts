b987331ca05492c4206fc8ebce46c91642a10bd2
윤병서 <bsyun0571@naver.com>
2024-09-30 15:04:59 +0900

    fix: conflict 해결

--- frontend/melodiket/public/icons/index.ts
+++ frontend/melodiket/public/icons/index.ts
@@ -1,61 +1,57 @@
 import LogoText from './logo-text.svg';
 import LogoImage from './logo-image.svg';
+export { LogoText, LogoImage };
+
 import MenuLine from './menu-line.svg';
 import SearchLine from './search-line.svg';
 import Arrow from './arrow-filled.svg';
+import Cross from './cross.svg';
+export { MenuLine, SearchLine, Arrow, Cross };
+
+import Home from './home.svg';
+import Notification from './notification.svg';
+import RoundQueueMusic from './round-queue-music.svg';
+import Guitar from './guitar.svg';
+import Favorite from './favorite.svg';
+import Basket from './basket.svg';
+import Card from './card.svg';
+import Microphone from './microphone.svg';
+import Music from './music.svg';
+import MyPage from './my-page.svg';
+import BackLine from './back-line.svg';
+import ForwardLine from './forward-line.svg';
+export {
+  Home,
+  Notification,
+  RoundQueueMusic,
+  Guitar,
+  Favorite,
+  Basket,
+  Card,
+  Microphone,
+  Music,
+  MyPage,
+  BackLine,
+  ForwardLine,
+};
+
 import Heart from './heart.svg';
-import QrCode from './qr-code.svg';
-import Ticket from './ticket.svg';
 import Check from './check.svg';
 import Calendar from './calendar.svg';
 import Help from './help.svg';
 import Upload from './upload.svg';
+export { Heart, Check, Calendar, Help, Upload };
+
+import MLDY from './MLDY.svg';
+import Authority from './authority.svg';
+export { MLDY, Authority };
+
+import QrCode from './qr-code.svg';
+import Ticket from './ticket-outline.svg';
 import CalendarFilled from './calendar-filled.svg';
 import Location from './location-filled.svg';
 import SubwayMark from './subway-mark.svg';
-import RoundQueueMusic from './round-queue-music.svg';
-import Home from './home.svg';
-import Notification from './notification.svg';
-import BackLine from './back-line.svg';
-import ForwardLine from './forward-line.svg';
-import Basket from './basket.svg';
-import Card from './card.svg';
-import Favorite from './favorite.svg';
-import Guitar from './guitar.svg';
-import Microphone from './microphone.svg';
-import Music from './music.svg';
-import MyPage from './my-page.svg';
-import MLDY from './mldy.svg';
-import Authority from './authority.svg';
+export { QrCode, Ticket, CalendarFilled, Location, SubwayMark };
 
-export {
-  LogoText,
-  LogoImage,
-  MenuLine,
-  SearchLine,
-  QrCode,
-  Ticket,
-  Check,
-  Arrow,
-  Heart,
-  Calendar,
-  Help,
-  Upload,
-  CalendarFilled,
-  Location,
-  SubwayMark,
-  RoundQueueMusic,
-  Home,
-  Notification,
-  BackLine,
-  ForwardLine,
-  Basket,
-  Card,
-  Favorite,
-  Guitar,
-  Microphone,
-  Music,
-  MyPage,
-  MLDY,
-  Authority,
-};
+import TicketShape from './ticket-shape.svg';
+export { TicketShape };
