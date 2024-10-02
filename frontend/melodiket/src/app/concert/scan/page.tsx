124ee787d5425964cb87a5b8a7386bd867c15a52
allkong <allempti@gmail.com>
2024-10-02 13:30:40 +0900

    feat: SubHeader 닫기 버튼을 뒤로가기로 수정

---
+++ frontend/melodiket/src/app/concert/scan/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import SubHeader from '@/components/organisms/navigation/SubHeader';
+
+const Page = () => {
+  return (
+    <div>
+      <SubHeader title="입장 처리" />
+    </div>
+  );
+};
+
+export default Page;
