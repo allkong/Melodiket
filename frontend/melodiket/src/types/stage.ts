8d6b16ae128a8ac1bfbf3dc70511bbfb81c7017d
윤병서 <bsyun0571@naver.com>
2024-10-02 17:53:28 +0900

    feat: request, response 타입 생성

--- frontend/melodiket/src/types/stage.ts
+++ frontend/melodiket/src/types/stage.ts
@@ -1,3 +1,5 @@
+import { UUID } from 'crypto';
+
 export interface StageData {
   stageName: string;
   stageAddress: string;
@@ -5,4 +7,23 @@ export interface StageData {
   capacity?: number;
   numOfRow?: number;
   numOfCol?: number;
+}
+
+export interface RegisterStageRequest {
+  stageName: string;
+  stageAddress: string;
+  isStanding: boolean;
+  capacity: number;
+  numOfRow: number;
+  numOfCol: number;
+}
+
+export interface RegisterStageResponse {
+  uuid: UUID;
+  stageName: string;
+  stageAddress: string;
+  isStanding: boolean;
+  capacity: number;
+  numOfRow: number;
+  numOfCol: number;
 }
