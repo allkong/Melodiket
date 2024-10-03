10e55bcb1e5648008727aadcf45c3b2442d1a442
pakrsevin <psv980817@naver.com>
2024-10-03 19:01:35 +0900

    refactor: 커서기반페이징네이션 비즈니스로직 재구현

--- backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/concert/controller/ConcertController.java
+++ backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/concert/controller/ConcertController.java
@@ -4,7 +4,6 @@ import com.ssafy.jdbc.melodiket.common.controller.dto.CursorPagingReq;
 import com.ssafy.jdbc.melodiket.common.page.PageResponse;
 import com.ssafy.jdbc.melodiket.concert.controller.dto.ConcertResp;
 import com.ssafy.jdbc.melodiket.concert.controller.dto.CreateConcertReq;
-import com.ssafy.jdbc.melodiket.concert.controller.dto.FooConcertResp;
 import com.ssafy.jdbc.melodiket.concert.service.ConcertService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,7 +23,7 @@ public class ConcertController {
 
     @GetMapping
     public ResponseEntity<PageResponse<ConcertResp>> getConcerts(
-            CursorPagingReq cursorPagingReq){
+            CursorPagingReq cursorPagingReq) {
         PageResponse<ConcertResp> concerts = concertService.getConcerts(cursorPagingReq);
         return ResponseEntity.ok(concerts);
     }
@@ -42,44 +41,40 @@ public class ConcertController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> cancelConcert(@PathVariable UUID id, Principal principal) {
-        String loginId = principal.getName();
-        concertService.cancelConcert(id, loginId);
+    public ResponseEntity<Void> cancelConcert(@PathVariable UUID id) {
+        concertService.cancelConcert(id);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/{id}/approve")
-    public ResponseEntity<Void> approveConcert(@PathVariable("id") UUID concertId,
-                                               @RequestParam("musicianId") UUID musicianId) {
-        concertService.approveConcert(concertId, musicianId);
+    public ResponseEntity<Void> approveConcert(@PathVariable("id") UUID concertId, Principal principal) {
+        String loginId = principal.getName();
+        concertService.approveConcert(concertId, loginId);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/{id}/deny")
-    public ResponseEntity<Void> denyConcert(@PathVariable("id") UUID concertId,
-                                            @RequestParam("musicianId") UUID musicianId) {
-        concertService.denyConcert(concertId, musicianId);
+    public ResponseEntity<Void> denyConcert(@PathVariable("id") UUID concertId, Principal principal) {
+        String loginId = principal.getName();
+        concertService.denyConcert(concertId, loginId);
         return ResponseEntity.ok().build();
     }
 
-//    @GetMapping("/by-stage-managers/{id}")
-//    public ResponseEntity<PageResponse<ConcertResp>> getConcertsByStageManager(
-//            @PathVariable UUID id,
-//            @Valid CursorPagingReq cursorPagingReq) {
-//        ConcertResp concerts = concertService.getConcertsByStageManager(id, cursorPagingReq);
-//        return ResponseEntity.ok(concerts);
-//    }
-//
-//    @GetMapping("/by-stage/{id}")
-//    public ResponseEntity<FooConcertResp> getConcertsByStage(
-//            @PathVariable UUID id,
-//            @RequestParam(value = "isFirstPage", required = true) Boolean isFirstPage,
-//            @RequestParam(value = "lastUuid", required = false) UUID lastUuid,
-//            @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
-//            @RequestParam(value = "orderKey", defaultValue = "id") String orderKey,
-//            @RequestParam(value = "orderDirection", defaultValue = "asc") String orderDirection) {
-//
-//        FooConcertResp concerts = concertService.getConcertsByStageManager(id, isFirstPage, lastUuid, pageSize, orderKey, orderDirection);
-//        return ResponseEntity.ok(concerts);
-//    }
+    @GetMapping("/by-stage-managers/{id}")
+    public ResponseEntity<PageResponse<ConcertResp>> getConcertsByStageManager(
+            @PathVariable UUID id,
+            @Valid CursorPagingReq cursorPagingReq) {
+
+        PageResponse<ConcertResp> concerts = concertService.getConcertsByStageManager(id, cursorPagingReq);
+        return ResponseEntity.ok(concerts);
+    }
+
+    @GetMapping("/by-stage/{id}")
+    public ResponseEntity<PageResponse<ConcertResp>> getConcertsByStage(
+            @PathVariable UUID id,
+            @Valid CursorPagingReq cursorPagingReq) {
+
+        PageResponse<ConcertResp> concerts = concertService.getConcertsByStage(id, cursorPagingReq);
+        return ResponseEntity.ok(concerts);
+    }
 }
