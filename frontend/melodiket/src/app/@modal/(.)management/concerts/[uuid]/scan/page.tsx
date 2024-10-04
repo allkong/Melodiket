65601d3b80e429a7d00fa51d1106cceca109a1e6
Dabin Jeong <allempti@gmail.com>
2024-10-05 02:51:44 +0900

    feat: 티켓 사용 처리 모달 정보 띄우기

--- frontend/melodiket/src/app/@modal/(.)management/concerts/[uuid]/scan/page.tsx
+++ frontend/melodiket/src/app/@modal/(.)management/concerts/[uuid]/scan/page.tsx
@@ -2,17 +2,39 @@
 
 import { useSearchParams } from 'next/navigation';
 
+import { useTicketDetail } from '@/services/ticket/useTicketdetail';
+import { formatSeatPosition } from '@/utils/concertFormatter';
+
 import ConfirmModal from '@/components/organisms/modal/ConfirmModal';
+import TicketInfo from '@/components/atoms/text/TicketInfo';
 
 const Modal = () => {
   const searchParams = useSearchParams();
   const ticketUuid = searchParams.get('ticket');
 
+  const { data: ticket } = useTicketDetail();
+
+  const ticketInfo = [
+    { label: '공연명', value: ticket?.concertTitle || '' },
+    { label: '예매자', value: '정다빈' },
+    {
+      label: '좌석',
+      value:
+        ticket?.seatRow && ticket?.seatCol
+          ? formatSeatPosition(ticket.seatRow, ticket.seatCol)
+          : '정보 없음',
+    },
+  ];
+
   return (
     <div>
       {ticketUuid && (
-        <ConfirmModal title="제목" onOk={() => alert('확인')}>
-          <p>내용</p>
+        <ConfirmModal
+          type="info"
+          title="티켓 사용 처리"
+          onOk={() => alert('확인')}
+        >
+          <TicketInfo fields={ticketInfo} size="sm" />
         </ConfirmModal>
       )}
     </div>
