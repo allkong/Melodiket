e8143c98b7330a1cd78858624713f70fe4b58ad8
강혁준 <domohit5124@naver.com>
2024-10-04 10:56:20 +0900

    refactor: sign-up 및 공연 예매 페이지 Funnel UI 변경

--- frontend/melodiket/src/app/concert/book/[uuid]/page.tsx
+++ frontend/melodiket/src/app/concert/book/[uuid]/page.tsx
@@ -8,6 +8,7 @@ import SeatSection from './_components/seat-section';
 import ConfirmSection from './_components/confirm-section';
 import { useFetchConcertDetail } from '@/services/concert/fetchConcert';
 import SuccessSection from './_components/success-section';
+import useFunnel from '@/hooks/useFunnel';
 
 const Page = () => {
   const params = useParams<{ uuid: string }>();
@@ -22,38 +23,44 @@ const Page = () => {
       favoriteMusician: '',
     });
 
-  const [step, setStep] = useState<'SEAT' | 'CONFIRM' | 'SUCCESS'>('SEAT');
+  const { Funnel, setStep } = useFunnel<'seat' | 'confirm' | 'success'>(
+    true,
+    true,
+    'seat'
+  );
 
   return (
     <div className="w-full h-full bg-gray-100">
-      {step === 'SEAT' && (
-        <SeatSection
-          onNext={(data) => {
-            setTicketBookInformation((prev) => ({ ...prev, ...data }));
-            setStep('CONFIRM');
-          }}
-        />
-      )}
-      {step === 'CONFIRM' && (
-        <ConfirmSection
-          seatRow={ticketBookInformation.seatRow}
-          seatCol={ticketBookInformation.seatCol}
-          onNext={(favoriteMusician) => {
-            /* eslint-disable @typescript-eslint/no-unused-vars */
-            const data: TicketBook = {
-              ...ticketBookInformation,
-              favoriteMusician,
-            };
-            setStep('SUCCESS');
-          }}
-        />
-      )}
-      {step === 'SUCCESS' && (
-        <SuccessSection
-          seatRow={ticketBookInformation.seatRow}
-          seatCol={ticketBookInformation.seatCol}
-        />
-      )}
+      <Funnel>
+        <Funnel.Step step="seat">
+          <SeatSection
+            onNext={(data) => {
+              setTicketBookInformation((prev) => ({ ...prev, ...data }));
+              setStep('confirm');
+            }}
+          />
+        </Funnel.Step>
+        <Funnel.Step step="confirm">
+          <ConfirmSection
+            seatRow={ticketBookInformation.seatRow}
+            seatCol={ticketBookInformation.seatCol}
+            onNext={(favoriteMusician) => {
+              /* eslint-disable @typescript-eslint/no-unused-vars */
+              const data: TicketBook = {
+                ...ticketBookInformation,
+                favoriteMusician,
+              };
+              setStep('success');
+            }}
+          />
+        </Funnel.Step>
+        <Funnel.Step step="success">
+          <SuccessSection
+            seatRow={ticketBookInformation.seatRow}
+            seatCol={ticketBookInformation.seatCol}
+          />
+        </Funnel.Step>
+      </Funnel>
     </div>
   );
 };
