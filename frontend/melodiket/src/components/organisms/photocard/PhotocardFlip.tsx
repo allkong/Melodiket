d05398444a24340679ef3853c1d5561a760879ea
Dabin Jeong <allempti@gmail.com>
2024-10-06 08:34:23 +0900

    feat: PhotocardFlip 컴포넌트 구현

---
+++ frontend/melodiket/src/components/organisms/photocard/PhotocardFlip.tsx
@@ -0,0 +1,70 @@
+import Image from 'next/image';
+
+import { formatDateCustom } from '@/utils/dayjsPlugin';
+import { formatDetailedSeatPosition } from '@/utils/concertFormatter';
+
+import { Heart, LogoImage, LogoText } from '@/public/icons';
+
+interface PhotocardFlipProps {
+  src: string;
+  concertName: string;
+  startAt: string;
+  stageName: string;
+  seatRow: number;
+  seatCol: number;
+}
+
+const PhotocardFlip = ({
+  src,
+  concertName,
+  startAt,
+  stageName,
+  seatRow,
+  seatCol,
+}: PhotocardFlipProps) => {
+  return (
+    <div>
+      {/* 포토카드 앞면 */}
+      <div className="w-[20.7rem] h-[31rem] relative rounded-lg overflow-hidden border border-gray-200">
+        <Image src={src} alt="photocard" fill className="object-cover" />
+      </div>
+
+      {/* 포토카드 뒷면 */}
+      <div className="w-[20.7rem] h-[31rem] relative rounded-lg overflow-hidden border border-gray-200 bg-white flex flex-col py-10 px-5 justify-between">
+        <section className="py-6 border-t border-b border-black">
+          <h1 className="text-xl font-semibold line-clamp-1">{concertName}</h1>
+        </section>
+
+        <div className="space-y-8">
+          <section className="space-y-1">
+            <p>{formatDateCustom(startAt, 'YYYY.MM.DD(ddd)')}</p>
+            <p>{formatDateCustom(startAt, 'HH:MM')}</p>
+          </section>
+
+          <section className="space-y-1">
+            <p>{stageName}</p>
+            <p>{formatDetailedSeatPosition(seatRow, seatCol)}</p>
+          </section>
+
+          <section className="space-y-1">
+            <p>카리나, 윈터, 닝닝, 지젤</p>
+            <div className="flex space-x-1.5 items-center">
+              <Heart
+                className="w-[1.1rem] h-auto text-black"
+                fill="currentColor"
+              />
+              <p>윈터</p>
+            </div>
+          </section>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <LogoImage className="mb-3 w-[1.4rem] h-[1.4rem]" />
+          <LogoText className="w-[4.5rem] h-auto" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PhotocardFlip;
