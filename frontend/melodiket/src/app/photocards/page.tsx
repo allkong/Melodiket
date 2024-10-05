c3114f7fc1f683e432e0e3eca9d08a7d86574296
Dabin Jeong <allempti@gmail.com>
2024-10-06 06:58:08 +0900

    feat: 포토카드 목록 페이지 구현

--- frontend/melodiket/src/app/photocards/page.tsx
+++ frontend/melodiket/src/app/photocards/page.tsx
@@ -1,11 +1,25 @@
-import PageTitle from '@/components/molecules/title/PageTitle';
+import { Suspense } from 'react';
+import { HydrationBoundary } from '@tanstack/react-query';
+
+import { usePhotocardListDehydrateState } from '@/services/photocard/usePhotocardList';
+
 import Header from '@/components/organisms/navigation/Header';
+import PageTitle from '@/components/molecules/title/PageTitle';
+import PhotocardListSection from './_components/photocard-list-section';
+import PhotocardListSkeleton from './_components/photocard-list-skeleton';
 
 const Page = () => {
   return (
     <div>
       <Header />
       <PageTitle title="포토카드" total={4} />
+      <div>
+        <HydrationBoundary state={usePhotocardListDehydrateState()}>
+          <Suspense fallback={<PhotocardListSkeleton count={2} />}>
+            <PhotocardListSection />
+          </Suspense>
+        </HydrationBoundary>
+      </div>
     </div>
   );
 };
