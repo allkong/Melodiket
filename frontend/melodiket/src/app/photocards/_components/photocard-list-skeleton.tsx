9d95411a7dd8d317bdc32d8928649e6257d25606
Dabin Jeong <allempti@gmail.com>
2024-10-06 06:53:32 +0900

    feat: PhotocardListSkeleton 구현

---
+++ frontend/melodiket/src/app/photocards/_components/photocard-list-skeleton.tsx
@@ -0,0 +1,20 @@
+import Skeleton from '@/components/atoms/skeleton/Skeleton';
+
+interface PhotocardListSkeletonProps {
+  count?: number;
+}
+
+const PhotocardListSkeleton = ({ count = 1 }: PhotocardListSkeletonProps) => {
+  return (
+    <div className="grid grid-flow-row grid-cols-2 lg:grid-cols-3 place-items-center w-full px-3 mt-2">
+      {Array.from({ length: count }, (_, idx) => idx).map((idx) => (
+        <div key={idx} className="space-y-2 px-1 py-1">
+          <Skeleton width={170} height={240} />
+          <Skeleton width={170} height={18} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PhotocardListSkeleton;
