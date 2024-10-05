eaea82a64b1ce76e743ed41df06fa0ff6a08de15
Dabin Jeong <allempti@gmail.com>
2024-10-06 02:02:42 +0900

    style: _component -> _components 폴더명 맞추기

--- frontend/melodiket/src/app/concerts/page.tsx
+++ frontend/melodiket/src/app/concerts/page.tsx
@@ -1,13 +1,13 @@
 import dynamic from 'next/dynamic';
 
 import Header from '@/components/organisms/navigation/Header';
-import ConcertListSection from './_component/concert-list-section';
+import ConcertListSection from './_components/concert-list-section';
 import { HydrationBoundary } from '@tanstack/react-query';
 import {
   useFetchCarouselListDehydrateState,
   useFetchConcertListDehydrateState,
 } from '@/services/concert/fetchConcert';
-import CarouselSection from './_component/carousel-section';
+import CarouselSection from './_components/carousel-section';
 
 const ControlsBar = dynamic(
   () => import('@/components/organisms/controls/ControlsBar'),
