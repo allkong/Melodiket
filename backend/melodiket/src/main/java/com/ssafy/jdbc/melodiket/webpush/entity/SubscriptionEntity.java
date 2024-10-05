48a3c5c75d0ba417b8dc49c833bb71e644b3bfb0
kkh <kkho9654@naver.com>
2024-10-05 22:38:47 +0900

    Feat: web push 모듈

---
+++ backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/webpush/entity/SubscriptionEntity.java
@@ -0,0 +1,29 @@
+package com.ssafy.jdbc.melodiket.webpush.entity;
+
+
+import com.ssafy.jdbc.melodiket.common.base.BaseEntity;
+import com.ssafy.jdbc.melodiket.user.entity.AppUserEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class SubscriptionEntity extends BaseEntity {
+    @Lob
+    private String endpoint;
+
+    @Lob
+    private String p256dhKey;
+
+    @Lob
+    private String auth;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private AppUserEntity appUserEntity;
+}
