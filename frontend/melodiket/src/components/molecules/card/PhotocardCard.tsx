24ba85214f8c99ceceae5853661705ba5a7e278e
Dabin Jeong <allempti@gmail.com>
2024-10-06 06:51:36 +0900

    feat: PhotocardCard 이미지 placeholder 처리

--- frontend/melodiket/src/components/molecules/card/PhotocardCard.tsx
+++ frontend/melodiket/src/components/molecules/card/PhotocardCard.tsx
@@ -1,22 +1,27 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
 interface PhotocardCardProps {
-  href: string;
   src: string;
   title: string;
 }
 
-const PhotocardCard = ({ href, src, title }: PhotocardCardProps) => {
+const PhotocardCard = ({ src, title }: PhotocardCardProps) => {
+  const blurDataUrl =
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAQAAAAnOwc2AAAAEUlEQVR42mO8/p8BAzAOZUEAMf0SZ24GauoAAAAASUVORK5CYII=';
   return (
-    <Link href={href || ''}>
-      <div className="w-[10.6rem] space-y-2">
-        <div className="relative w-full h-60 overflow-hidden rounded-md">
-          <Image src={src} alt="photocard" className="object-cover" fill />
-        </div>
-        <p className="font-medium text-xs line-clamp-1 w-full">{title}</p>
+    <div className="w-[10.6rem] space-y-2">
+      <div className="relative w-full h-60 overflow-hidden rounded-md">
+        <Image
+          src={src}
+          alt="photocard"
+          className="object-cover"
+          fill
+          placeholder="blur"
+          blurDataURL={blurDataUrl}
+        />
       </div>
-    </Link>
+      <p className="font-medium text-xs line-clamp-1 w-full">{title}</p>
+    </div>
   );
 };
 
