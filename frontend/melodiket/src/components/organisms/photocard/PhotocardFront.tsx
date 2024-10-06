c1b4dc18c4324994ea1cac6a670bbf0523cf2ebf
Dabin Jeong <allempti@gmail.com>
2024-10-06 19:13:25 +0900

    feat: 포토카드 공유 페이지 구현

--- frontend/melodiket/src/components/organisms/photocard/PhotocardFront.tsx
+++ frontend/melodiket/src/components/organisms/photocard/PhotocardFront.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import { getBlurDataUrl } from '@/utils/getUrl';
+
 import RotateButton from '@/components/atoms/button/RotateButton';
 
 interface PhotocardFrontProps {
@@ -10,7 +12,14 @@ interface PhotocardFrontProps {
 const PhotocardFront = ({ src, onClick }: PhotocardFrontProps) => {
   return (
     <div className="w-[20.7rem] h-[33.3rem] relative rounded-lg overflow-hidden border border-gray-200">
-      <Image src={src} alt="photocard" fill className="object-cover" />
+      <Image
+        src={src}
+        alt="photocard"
+        fill
+        className="object-cover"
+        placeholder="blur"
+        blurDataURL={getBlurDataUrl()}
+      />
 
       <div className="absolute bottom-0 right-0 px-3 py-3">
         <RotateButton onClick={onClick} />
