a45b4287d7ca6a7bee674dca9e302d669296b08f
Dabin Jeong <allempti@gmail.com>
2024-10-06 10:28:37 +0900

    feat: PhotocardDetail 타입 지정

--- frontend/melodiket/src/types/photocard.ts
+++ frontend/melodiket/src/types/photocard.ts
@@ -8,3 +8,19 @@ export interface Photocard {
 export interface PhotocardList {
   result: Photocard[];
 }
+
+export interface PhotocardDetail {
+  photocardUuid: string;
+  ticketUuid?: string;
+  concertUuid?: string;
+  concertName: string;
+  imageCid: string;
+  createdAt: string;
+  startAt: string;
+  stageName: string;
+  seatRow: number;
+  seatCol: number;
+  musicans: string[];
+  favoriteMusician: string;
+  nickname: string;
+}
