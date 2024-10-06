7cb35fef828cec214c0ef434fcf5f21cccbef954
Dabin Jeong <allempti@gmail.com>
2024-10-06 14:34:50 +0900

    refactor: LargeButton에서 CloseButton으로 사용 변경

--- frontend/melodiket/src/app/photocards/[uuid]/page.tsx
+++ frontend/melodiket/src/app/photocards/[uuid]/page.tsx
@@ -1,23 +1,31 @@
 'use client';
 
+import CloseButton from '@/components/atoms/button/CloseButton';
 import LargeButton from '@/components/atoms/button/LargeButton';
 import Header from '@/components/organisms/navigation/Header';
+import SubHeader from '@/components/organisms/navigation/SubHeader';
 import PhotocardFlip from '@/components/organisms/photocard/PhotocardFlip';
 import { usePhotocardDetail } from '@/services/photocard/usePhotocardDetail';
 import { formatDateToYMD } from '@/utils/dayjsPlugin';
+import { useRouter } from 'next/navigation';
 
 const Page = () => {
+  const router = useRouter();
   const { data: photocard } = usePhotocardDetail();
 
   if (!photocard) {
     return null;
   }
 
+  const handleClose = () => {
+    router.back();
+  };
+
   return (
     <div className="flex flex-col h-screen overflow-y-auto">
       <Header />
-      <div className="flex flex-col items-center justify-between flex-grow">
-        <section className="flex flex-col items-center">
+      <div className="flex flex-col items-center py-8">
+        <section className="flex flex-col items-center mb-9">
           <h1 className="flex text-lg">
             <p className="font-semibold">{photocard.nickname}</p>님의 포토카드
           </h1>
@@ -40,9 +48,7 @@ const Page = () => {
           />
         </div>
 
-        <div className="w-[20.7rem]">
-          <LargeButton label="예매 정보" />
-        </div>
+        <CloseButton onClick={handleClose} hasBorder />
       </div>
     </div>
   );
