96053c53dfebede16d1128208e8fe2f21d6c545d
Dabin Jeong <allempti@gmail.com>
2024-10-06 10:28:56 +0900

    feat: 포토카드 상세조회 데이터 요청

--- frontend/melodiket/src/services/photocard/photocardKey.ts
+++ frontend/melodiket/src/services/photocard/photocardKey.ts
@@ -1,6 +1,7 @@
 const photocardKey = {
   default: ['photocard'],
   list: () => [...photocardKey.default, 'list'],
+  detail: (uuid: string) => [...photocardKey.default, 'detail', uuid],
 } as const;
 
 export default photocardKey;
