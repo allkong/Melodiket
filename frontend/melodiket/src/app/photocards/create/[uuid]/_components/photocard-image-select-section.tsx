07033e9bb28e64a4ec805dddec1f08300de1f65f
Dabin Jeong <allempti@gmail.com>
2024-10-07 03:30:12 +0900

    feat: PhotocardImageSelectSection UI 구현

---
+++ frontend/melodiket/src/app/photocards/create/[uuid]/_components/photocard-image-select-section.tsx
@@ -0,0 +1,32 @@
+import MediumButton from '@/components/atoms/button/MediumButton';
+import PhotocardFrame from '@/components/organisms/photocard/PhotocardFrame';
+import { ImageLine, Ticket } from '@/public/icons';
+
+interface PhotocardImageSelectSection {
+  onNext: () => void;
+}
+
+const PhotocardImageSelectSection = ({
+  onNext,
+}: PhotocardImageSelectSection) => {
+  return (
+    <div className="flex flex-col items-center justify-center flex-grow">
+      <PhotocardFrame>
+        <div className="flex items-center justify-center w-full h-full bg-gray-100">
+          <p>사진에 들어갈 이미지를 선택해 주세요</p>
+        </div>
+      </PhotocardFrame>
+
+      <div className="flex mt-10 space-x-3">
+        <MediumButton label="공연 포스터" icon={<Ticket />} onClick={onNext} />
+        <MediumButton
+          label="이미지 선택"
+          icon={<ImageLine />}
+          onClick={() => alert('이미지 업로드')}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PhotocardImageSelectSection;
