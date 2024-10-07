ba3d222c854184c0f4f8fae037a22f702ca4323e
allkong <allempti@gmail.com>
2024-10-07 09:22:38 +0900

    refactor: Header 컴포넌트의 Menu children을 Menu 컴포넌트로 이동

--- frontend/melodiket/src/components/organisms/menu/Menu.tsx
+++ frontend/melodiket/src/components/organisms/menu/Menu.tsx
@@ -1,20 +1,55 @@
-import { ReactNode } from 'react';
-
 import MenuPortal from './MenuPortal';
 import MenuMain from './MenuMain';
 import ThinDivider from '@/components/atoms/divider/ThinDivider';
 import MenuHeader from './MenuHeader';
 import MenuItem from './MenuItem';
 import MenuProfile from './MenuProfile';
+import {
+  BackLine,
+  Basket,
+  Card,
+  Favorite,
+  Guitar,
+  Microphone,
+  Music,
+  MyPage,
+} from '@/public/icons';
 
-interface MenuWithPortalProps {
-  children?: ReactNode;
-}
-
-const MenuWithPortal = ({ children }: MenuWithPortalProps) => {
+const MenuWithPortal = () => {
   return (
     <MenuPortal>
-      <MenuMain>{children}</MenuMain>
+      <MenuMain>
+        <Menu.Header />
+        <Menu.Profile />
+        <Menu.Divider />
+        <Menu.Item href="/concerts" icon={<Music />} label="공연" />
+        <Menu.Item
+          href="/management/concerts"
+          icon={<Music />}
+          label="공연 생성"
+        />
+        <Menu.Item href="/musicians" icon={<Guitar />} label="뮤지션" />
+        <Menu.Divider />
+        <Menu.Item
+          href="/favorites"
+          icon={<Favorite />}
+          label="찜한 공연/뮤지션"
+        />
+        <Menu.Item href="/mytickets" icon={<Basket />} label="예매내역" />
+        <Menu.Item
+          href="/photocards"
+          icon={<Card width={22} height={22} className="text-purple-200" />}
+          label="포토카드"
+        />
+        <Menu.Item
+          href="/management/concerts"
+          icon={<Microphone />}
+          label="내 공연"
+        />
+        <Menu.Divider />
+        <Menu.Item href="/mypage" icon={<MyPage />} label="마이페이지" />
+        <Menu.Item href="/" icon={<BackLine />} label="로그아웃" />
+      </MenuMain>
     </MenuPortal>
   );
 };
