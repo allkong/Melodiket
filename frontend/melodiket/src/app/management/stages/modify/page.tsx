3b6a200a53477d72e25266eaffd228491b5899f8
윤병서 <bsyun0571@naver.com>
2024-10-07 14:14:56 +0900

    fix: overflow fix

--- frontend/melodiket/src/app/management/stages/modify/page.tsx
+++ frontend/melodiket/src/app/management/stages/modify/page.tsx
@@ -19,7 +19,7 @@ const Page = () => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <div className="flex-grow h-0 p-4">
+      <div className="flex-grow h-0 p-4 overflow-y-auto">
         <TextBanner
           title="수정할 공연장을 선택해주세요"
           description="공연장을 수정하면 되돌릴 수 없습니다"
