9e873fc8192dba966cde1234345d7413962db431
allkong <allempti@gmail.com>
2024-10-07 14:18:27 +0900

    refactor: Tabs 스토리북에서 useState

--- frontend/melodiket/src/components/organisms/controls/Tabs.stories.tsx
+++ frontend/melodiket/src/components/organisms/controls/Tabs.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Tabs from './Tabs';
+import { FAVORITE_TYPES } from '@/constants/favoriteTypes';
+import { useState } from 'react';
 
 const meta: Meta<typeof Tabs> = {
   component: Tabs,
@@ -9,7 +11,21 @@ export default meta;
 type Story = StoryObj<typeof Tabs>;
 
 export const Default: Story = {
-  args: {
-    tabs: ['concert', 'musician'],
+  render: (args) => {
+    const [activeTab, setActiveTab] = useState(Object.keys(FAVORITE_TYPES)[0]);
+
+    const handleTabClick = (tabValue: string) => {
+      setActiveTab(tabValue);
+    };
+
+    return (
+      <Tabs
+        {...args}
+        tabs={Object.keys(FAVORITE_TYPES)}
+        activeTab={activeTab}
+        onClick={handleTabClick}
+        labelMap={FAVORITE_TYPES}
+      />
+    );
   },
 };
