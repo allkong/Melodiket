5764b9a754bf4eebd32d14d60a27d614e69a7f85
윤병서 <bsyun0571@naver.com>
2024-10-07 13:32:03 +0900

    feat: 뮤지션 타입 생성

---
+++ frontend/melodiket/src/types/musician.ts
@@ -0,0 +1,22 @@
+export interface PageParam {
+  isFirstPage: boolean;
+  lastUuid?: string;
+}
+
+export interface Musician {
+  musicianUuid: string;
+  name: string;
+  imageUrl: string;
+}
+
+export interface PageInfo {
+  hasNextPage: boolean;
+  requestedSize: number;
+  responsedSize: number;
+  lastUuid: string;
+}
+
+export interface FetchMusiciansResponse {
+  pageInfo: PageInfo;
+  result: Musician[];
+}
