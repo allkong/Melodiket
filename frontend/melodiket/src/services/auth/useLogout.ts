14fcf834aa5a9e5d100165a1798c2c4205992f90
allkong <allempti@gmail.com>
2024-10-07 09:46:37 +0900

    feat: logout 요청

---
+++ frontend/melodiket/src/services/auth/useLogout.ts
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/navigation';
+import customFetch from '../customFetch';
+import useAuthStore from '@/store/authStore';
+
+const logout = async () => {
+  return await customFetch('/auth/logout', {
+    method: 'POST',
+  });
+};
+
+export const useLogout = () => {
+  const router = useRouter();
+  const { clearAuth } = useAuthStore();
+
+  return async () => {
+    try {
+      await logout();
+      clearAuth();
+      router.push('/');
+    } catch (error) {
+      alert('로그아웃 실패😥');
+    }
+  };
+};
