bfe0fbf773c6d5af73bb430c2073444eb8ba830b
강혁준 <domohit5124@naver.com>
2024-10-07 15:29:02 +0900

    feat: 메인화면 티켓 목록 조회

--- frontend/melodiket/src/app/page.tsx
+++ frontend/melodiket/src/app/page.tsx
@@ -1,12 +1,12 @@
 import { Suspense } from 'react';
 
 import Header from '@/components/organisms/navigation/Header';
-import TicketInfoCarousel from '@/components/molecules/carousel/TicketInfoCarousel';
 import ConcertRankingSection from './_section/concert-ranking-section';
 import FavoriteMusicianSection from './_section/favorite-musician-section';
 import CarouselSection from './_section/carousel-section';
 import NeedLogin from './_components/need-login';
 import ConcertRankingCardSkeleton from '@/components/molecules/card/ConcertRankingCardSkeleton';
+import TicketSection from './_section/ticket-section';
 
 export default function Home() {
   return (
@@ -15,9 +15,7 @@ export default function Home() {
       <div className="w-full mt-16 px-7">
         <CarouselSection />
         <div className="flex flex-col gap-6 my-3">
-          <TicketInfoCarousel
-            datas={['예매한 티켓이 없어요 T_T', '사실 잇지롱', 'A310 화이팅~']}
-          />
+          <TicketSection />
           <div>
             <p className="text-xl font-medium">공연 랭킹</p>
             <section className="w-full py-2 overflow-x-auto">
