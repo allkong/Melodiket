162785a0cf4a30bb7ad52cf3cb21fa9b1ca9a095
윤병서 <bsyun0571@naver.com>
2024-10-07 13:51:32 +0900

    feat: 마이페이지 경로 설정 및 권한 설정

--- frontend/melodiket/src/app/mypage/page.tsx
+++ frontend/melodiket/src/app/mypage/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import Link from 'next/link';
-
 import Header from '@/components/organisms/navigation/Header';
 import Profile from '@/components/atoms/profile/Profile';
 import MyPageItem from '@/components/molecules/item/MyPageItem';
@@ -9,8 +7,20 @@ import LineDivider from '@/components/atoms/divider/LineDivider';
 import LargeButton from '@/components/atoms/button/LargeButton';
 
 import { MLDY, Authority } from '@/public/icons';
+import useAuthStore from '@/store/authStore';
+import { useRouter } from 'next/navigation';
 
 const Page = () => {
+  const { user } = useAuthStore();
+  const router = useRouter();
+
+  const isStageManager = user?.role === 'STAGE_MANAGER';
+  const isAudience = user?.role === 'AUDIENCE';
+
+  const handleNavigation = (path: string) => {
+    router.push(path);
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
@@ -20,7 +30,9 @@ const Page = () => {
             <div className="flex items-center space-x-4 mb-4 p-4">
               <Profile size="md" />
               <div>
-                <p className="text-xl font-semibold text-black">햄스터왕</p>
+                <p className="text-xl font-semibold text-black">
+                  {user?.nickname}
+                </p>
                 <p className="text-sm text-gray-500">@deadbird00</p>
               </div>
             </div>
@@ -31,7 +43,7 @@ const Page = () => {
                   <Authority />
                   <p className="text-purple-400 font-medium">권한</p>
                 </div>
-                <p className="text-black ml-auto">뮤지션</p>
+                <p className="text-black ml-auto">{user?.role}</p>
               </div>
               <div className="flex items-center justify-between space-x-2">
                 <div className="flex items-center space-x-2">
@@ -47,15 +59,37 @@ const Page = () => {
             <MyPageItem label="계좌 수정" />
             <MyPageItem label="계좌 충전" />
             <LineDivider />
-            <MyPageItem label="공연장 등록하기" />
-            <MyPageItem label="공연장 수정하기" />
-            <LineDivider />
-            <MyPageItem label="정보 수정하기" />
-            <MyPageItem label="탈퇴하기" />
-            <LineDivider />
-            <MyPageItem label="찜한 공연" />
-            <MyPageItem label="찜한 뮤지션" />
-            <MyPageItem label="예매 내역" />
+            {isStageManager && (
+              <>
+                <MyPageItem
+                  label="공연장 등록하기"
+                  onClick={() =>
+                    handleNavigation('/management/stages/register')
+                  }
+                />
+                <MyPageItem
+                  label="공연장 삭제하기"
+                  onClick={() => handleNavigation('/management/stages/modify')}
+                />
+                <LineDivider />
+              </>
+            )}
+            {isAudience && (
+              <>
+                <MyPageItem label="찜한 공연" />
+                <MyPageItem label="찜한 뮤지션" />
+                <MyPageItem label="예매 내역" />
+                <LineDivider />
+              </>
+            )}
+            <MyPageItem
+              label="정보 수정하기"
+              onClick={() => handleNavigation('/mypage/modify')}
+            />
+            <MyPageItem
+              label="탈퇴하기"
+              onClick={() => handleNavigation('/mypage/withdrawal')}
+            />
           </div>
           <div className="my-4 h-fit p-4">
             <LargeButton label="로그아웃" />
