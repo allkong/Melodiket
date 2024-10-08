7bfb995534e05ddef1d6807e5566e6d50040bef2
Dabin Jeong <allempti@gmail.com>
2024-10-09 06:17:50 +0900

    feat: 뮤지션 상세 조회에서 뮤지션 찜 토글 구현

--- frontend/melodiket/src/app/musicians/[uuid]/page.tsx
+++ frontend/melodiket/src/app/musicians/[uuid]/page.tsx
@@ -1,13 +1,7 @@
-'use client';
-
 import { Suspense } from 'react';
 
-import { useMusicianDetail } from '@/services/musician/fetchMusician';
-import { getS3Url } from '@/utils/getUrl';
-
 import Header from '@/components/organisms/navigation/Header';
-import BackgroundFrame from '@/components/atoms/image-frame/BackgroundFrame';
-import FavoriteProfile from '@/components/molecules/profile/FavoriteProfile';
+import MusicianProfileSection from './_section/musician-profile-section';
 import ControlsBar from '@/components/organisms/controls/ControlsBar';
 import ControlsBarSkeleton from '@/components/organisms/controls/ControlsBarSkeleton';
 
@@ -16,26 +10,10 @@ interface PageProps {
 }
 
 const Page = ({ params }: PageProps) => {
-  const { data: musician } = useMusicianDetail(params.uuid);
-
   return (
     <div>
       <Header isFixed />
-      <div className="relative mb-20">
-        <BackgroundFrame src={getS3Url(musician?.imageUrl || '')} />
-        <div className="absolute transform -translate-x-1/2 -translate-y-1/2 left-1/2">
-          <FavoriteProfile src={getS3Url(musician?.imageUrl || '')} size="lg" />
-          <div className="absolute bottom-0 right-0 transform translate-x-8">
-            <p>{musician?.likeCount}</p>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col items-center mx-5">
-        <h1 className="text-2xl font-semibold">{musician?.nickname}</h1>
-        <div className="py-4 text-sm border-b w-full text-center">
-          <p>{musician?.description}</p>
-        </div>
-      </div>
+      <MusicianProfileSection musicianUuid={params.uuid} />
       <div className="w-full my-4">
         <h2 className="mx-5 text-lg font-medium">뮤지션의 공연</h2>
         <Suspense fallback={<ControlsBarSkeleton />}>
