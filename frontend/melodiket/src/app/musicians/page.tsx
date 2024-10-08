f6b232099b657dd01b9e1f00ff21f5de2d5e39be
allkong <allempti@gmail.com>
2024-10-08 16:52:40 +0900

    feat: 뮤지션 목록 api 연동

--- frontend/melodiket/src/app/musicians/page.tsx
+++ frontend/melodiket/src/app/musicians/page.tsx
@@ -1,9 +1,9 @@
 import CAROUSEL_DATAS from '@/constants/carousel';
 
 import Header from '@/components/organisms/navigation/Header';
-import MusicianItem from '@/components/molecules/item/MusicianItem';
 import Carousel from '@/components/molecules/carousel/Carousel';
 import ControlsBar from '@/components/organisms/controls/ControlsBar';
+import MusicianListSection from './_section/musician-list-section';
 
 const Page = () => {
   return (
@@ -11,15 +11,7 @@ const Page = () => {
       <Header isFixed />
       <Carousel data={CAROUSEL_DATAS} />
       <ControlsBar />
-      <div>
-        <MusicianItem
-          src={''}
-          musicianName={'장원영'}
-          favoriteCount={10}
-          bookingCount={2}
-          isFavorite
-        />
-      </div>
+      <MusicianListSection />
     </div>
   );
 };
