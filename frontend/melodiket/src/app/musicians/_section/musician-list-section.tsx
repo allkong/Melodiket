ecdc248d5eebe47815b8958c90690d25227cb414
allkong <allempti@gmail.com>
2024-10-08 17:14:25 +0900

    feat: 뮤지션 목록 무한스크롤 적용

--- frontend/melodiket/src/app/musicians/_section/musician-list-section.tsx
+++ frontend/melodiket/src/app/musicians/_section/musician-list-section.tsx
@@ -1,30 +1,53 @@
 'use client';
 
-import MusicianItem from '@/components/molecules/item/MusicianItem';
+import { useEffect, useRef } from 'react';
+
 import { useMusiciansQuery } from '@/services/musician/fetchMusicians';
 import { getS3Url } from '@/utils/getUrl';
+import useIsOnScreen from '@/hooks/useIsOnScreen';
+
+import MusicianItem from '@/components/molecules/item/MusicianItem';
+import MusicianItemSkeleton from '@/components/molecules/item/MusicianItemSkeleton';
+import IsEnd from '@/app/concerts/_components/is-end';
+import IsError from '@/app/concerts/_components/is-error';
 
 const MusicianListSection = () => {
-  const { data } = useMusiciansQuery();
+  const { data, isFetching, error, hasNextPage, fetchNextPage, refetch } =
+    useMusiciansQuery();
   const { pages } = data ?? {};
 
+  const endRef = useRef<HTMLDivElement>(null);
+  const isOnScreen = useIsOnScreen(endRef);
+
+  useEffect(() => {
+    if (isOnScreen && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [isOnScreen, hasNextPage, fetchNextPage]);
+
   return (
-    <div>
-      {pages &&
-        pages
-          ?.flatMap((page) => page.result)
-          .map((musician) => (
-            <MusicianItem
-              href={`/musicians/${musician.uuid}`}
-              key={musician.uuid}
-              src={getS3Url(musician.imageUrl)}
-              musicianName={musician.nickname}
-              favoriteCount={musician.likeCount}
-              bookingCount={2}
-              isFavorite
-            />
-          ))}
-    </div>
+    <>
+      <div>
+        {pages &&
+          pages
+            ?.flatMap((page) => page.result)
+            .map((musician) => (
+              <MusicianItem
+                href={`/musicians/${musician.uuid}`}
+                key={musician.uuid}
+                src={getS3Url(musician.imageUrl)}
+                musicianName={musician.nickname}
+                favoriteCount={musician.likeCount}
+                bookingCount={2}
+                isFavorite
+              />
+            ))}
+        {isFetching && <MusicianItemSkeleton count={5} />}
+      </div>
+      {error && <IsError onClick={refetch} />}
+      {!hasNextPage && <IsEnd />}
+      <div ref={endRef} />
+    </>
   );
 };
 
