f0e71a0b8c45fd8c3d6b88668a81a69ccbb24f92
박유빈 <qkrdbqls1001@naver.com>
2024-10-08 13:26:54 +0900

    hotfix: import error

--- backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/concert/controller/ConcertController.java
+++ backend/melodiket/src/main/java/com/ssafy/jdbc/melodiket/concert/controller/ConcertController.java
@@ -2,10 +2,7 @@ package com.ssafy.jdbc.melodiket.concert.controller;
 
 import com.ssafy.jdbc.melodiket.common.controller.dto.CursorPagingReq;
 import com.ssafy.jdbc.melodiket.common.page.PageResponse;
-import com.ssafy.jdbc.melodiket.concert.controller.dto.ConcertAssignmentResp;
-import com.ssafy.jdbc.melodiket.concert.controller.dto.ConcertCursorPagingReq;
-import com.ssafy.jdbc.melodiket.concert.controller.dto.ConcertResp;
-import com.ssafy.jdbc.melodiket.concert.controller.dto.CreateConcertReq;
+import com.ssafy.jdbc.melodiket.concert.controller.dto.*;
 import com.ssafy.jdbc.melodiket.concert.service.ConcertService;
 import com.ssafy.jdbc.melodiket.user.entity.AppUserEntity;
 import jakarta.validation.Valid;
