bd7ed177fc2d236b7b7f92669af34122aa791844
allkong <allempti@gmail.com>
2024-10-08 11:05:21 +0900

    feat: s3, cid 이미지 파일 앞에 url 붙여주는 util 함수 구현

--- frontend/melodiket/src/utils/getUrl.ts
+++ frontend/melodiket/src/utils/getUrl.ts
@@ -1,3 +1,13 @@
 export const getBlurDataUrl = (): string => {
   return 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAQAAAAnOwc2AAAAEUlEQVR42mO8/p8BAzAOZUEAMf0SZ24GauoAAAAASUVORK5CYII=';
 };
+
+export const getS3Url = (filename: string) => {
+  const baseUrl = process.env.NEXT_PUBLIC_S3_URL;
+  return `${baseUrl}/${filename}`;
+};
+
+export const getCidUrl = (filename: string) => {
+  const baseUrl = process.env.NEXT_PUBLIC_CID_URL;
+  return `${baseUrl}/${filename}`;
+};
