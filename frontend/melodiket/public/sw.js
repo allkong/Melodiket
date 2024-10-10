67a5dbdb79526923a8a9eb7c8e47ae934c671ee3
강혁준 <domohit5124@naver.com>
2024-10-11 03:59:40 +0900

    remove: log 제거

--- frontend/melodiket/public/sw.js
+++ frontend/melodiket/public/sw.js
@@ -17,7 +17,7 @@ self.addEventListener('push', function (event) {
 });
 
 self.addEventListener('notificationclick', function (event) {
-  console.log('Notification click received.');
+  // console.log('Notification click received.');
   event.notification.close();
   event.waitUntil(clients.openWindow('<https://your-website.com>'));
 });
