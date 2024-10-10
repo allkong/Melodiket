9e2597c210507cac2c9445dd81975756acd6e93c
allkong <allempti@gmail.com>
2024-10-10 15:10:45 +0900

    feat: 포토카드 텍스트 추가

---
+++ frontend/melodiket/src/app/photocards/create/[uuid]/_components/moveable-text.tsx
@@ -0,0 +1,56 @@
+import { SelectText } from '@/types/photocard';
+import { useRef } from 'react';
+import Moveable from 'react-moveable';
+
+interface MoveableTextProps {
+  text: SelectText;
+  isSelected: boolean;
+  onSelect: () => void;
+  containerRef: React.RefObject<HTMLElement>;
+}
+
+const MoveableText = ({
+  text,
+  isSelected,
+  onSelect,
+  containerRef,
+}: MoveableTextProps) => {
+  const textRef = useRef<HTMLDivElement | null>(null);
+
+  return (
+    <>
+      <div
+        ref={textRef}
+        onClick={onSelect}
+        className="absolute cursor-pointer"
+        style={{ left: `${text.x}px`, top: `${text.y}px` }}
+      >
+        <p className={`text-[${text.color}]`}>{text.label}</p>
+      </div>
+
+      {isSelected && (
+        <Moveable
+          target={textRef.current}
+          container={containerRef.current}
+          draggable={true}
+          onDrag={({ target, transform }) => {
+            target!.style.transform = transform;
+          }}
+          keepRatio={true}
+          scalable={true}
+          onScale={({ target, transform }) => {
+            target!.style.transform = transform;
+          }}
+          rotatable={true}
+          onRotate={({ target, transform }) => {
+            target!.style.transform = transform;
+          }}
+          pinchable={true}
+          pinchOutside={true}
+        />
+      )}
+    </>
+  );
+};
+
+export default MoveableText;
