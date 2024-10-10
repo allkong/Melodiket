6b76116a1d2c001c601857a6ca855d37da7e4810
allkong <allempti@gmail.com>
2024-10-11 00:24:54 +0900

    tmp

--- frontend/melodiket/src/constants/photocard.ts
+++ frontend/melodiket/src/constants/photocard.ts
@@ -5,7 +5,16 @@ export const PHOTOCARD_EDIT_TYPES = {
 
 export const STICKER_CATEGORIES = {
   favorite: '최애',
+  member: 'JDBC',
   animal: '동물',
+  instrument: '악기',
+  pixel: '픽셀',
   heart: '하트',
+  ocean: '바다',
+  emoji: '표정',
   fruit: '과일',
+  flower: '꽃',
+  real_hamster: '햄스터',
+  real_cat: '고양이',
+  christmas: '크리스마스',
 } as const;
