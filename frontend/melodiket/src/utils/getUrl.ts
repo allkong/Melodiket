24840133de83c4db1ea683157c7dc96a62940671
강혁준 <domohit5124@naver.com>
2024-10-11 03:47:38 +0900

    fix: 홈화면 오류 수정

--- frontend/melodiket/src/utils/getUrl.ts
+++ frontend/melodiket/src/utils/getUrl.ts
@@ -2,8 +2,8 @@ export const getBlurDataUrl = (): string => {
   return 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAQAAAAnOwc2AAAAEUlEQVR42mO8/p8BAzAOZUEAMf0SZ24GauoAAAAASUVORK5CYII=';
 };
 
-export const getS3Url = (filename: string) => {
-  if (filename === '') {
+export const getS3Url = (filename: string | null) => {
+  if (filename === '' || filename === null) {
     return '';
   }
 
