ff9a66e0c19566d690ad3b2608d5a4a2d7fbd366
강혁준 <domohit5124@naver.com>
2024-10-10 15:20:56 +0900

    feat: 트랜잭션 페이지 구현 및 메뉴 추가

--- frontend/melodiket/src/types/transaction.ts
+++ frontend/melodiket/src/types/transaction.ts
@@ -1,16 +1,8 @@
 export interface TransactionResponse {
-  pageInfo: {
-    hasNextPage: boolean;
-    requestedSize: number;
-    responsedSize: number;
-    lastId: string;
-  };
-  result: {
-    id: string;
-    contractAddress: string;
-    blockHeight: number;
-    eventName: string;
-    transactionHash: string;
-    timestamp: string;
-  }[];
+  id: string;
+  contractAddress: string;
+  blockHeight: number;
+  eventName: string;
+  transactionHash: string;
+  timestamp: string;
 }
