3b7124bbfd61faafbec2476acf3ce90c37b356ab
allkong <allempti@gmail.com>
2024-10-11 01:43:06 +0900

    feat: 스티커 연결

--- frontend/melodiket/src/app/@modal/(.)photocards/create/[uuid]/_components/Stickers.tsx
+++ frontend/melodiket/src/app/@modal/(.)photocards/create/[uuid]/_components/Stickers.tsx
@@ -12,156 +12,25 @@ const StickerImage = ({ src }: StickerImageProps) => {
   return <Image src={src} alt="sticker" width={80} height={80} />;
 };
 
+const createStickers = (category: string, count: number): Sticker[] => {
+  return Array.from({ length: count }, (_, index) => ({
+    name: `${category}${index + 1}`,
+    category,
+    image: <StickerImage src={`/stickers/${category}/${index + 1}.png`} />,
+  }));
+};
+
 export const stickers: Sticker[] = [
   { name: 'signature', category: 'favorite', image: <MusicianSignature /> },
-  {
-    name: 'animal1',
-    category: 'animal',
-    image: <StickerImage src="/stickers/animal/1.png" />,
-  },
-  {
-    name: 'animal2',
-    category: 'animal',
-    image: <StickerImage src="/stickers/animal/2.png" />,
-  },
-  {
-    name: 'animal3',
-    category: 'animal',
-    image: <StickerImage src="/stickers/animal/3.png" />,
-  },
-  {
-    name: 'animal4',
-    category: 'animal',
-    image: <StickerImage src="/stickers/animal/4.png" />,
-  },
-  {
-    name: 'animal5',
-    category: 'animal',
-    image: <StickerImage src="/stickers/animal/5.png" />,
-  },
-  {
-    name: 'animal6',
-    category: 'animal',
-    image: <StickerImage src="/stickers/animal/6.png" />,
-  },
-  {
-    name: 'animal7',
-    category: 'animal',
-    image: <StickerImage src="/stickers/animal/7.png" />,
-  },
-  {
-    name: 'animal8',
-    category: 'animal',
-    image: <StickerImage src="/stickers/animal/8.png" />,
-  },
-  {
-    name: 'animal9',
-    category: 'animal',
-    image: <StickerImage src="/stickers/animal/9.png" />,
-  },
-  {
-    name: 'animal10',
-    category: 'animal',
-    image: <StickerImage src="/stickers/animal/10.png" />,
-  },
-  {
-    name: 'christmas1',
-    category: 'christmas',
-    image: <StickerImage src="/stickers/christmas/1.png" />,
-  },
-  {
-    name: 'christmas2',
-    category: 'christmas',
-    image: <StickerImage src="/stickers/christmas/2.png" />,
-  },
-  {
-    name: 'christmas3',
-    category: 'christmas',
-    image: <StickerImage src="/stickers/christmas/3.png" />,
-  },
-  {
-    name: 'christmas4',
-    category: 'christmas',
-    image: <StickerImage src="/stickers/christmas/4.png" />,
-  },
-  {
-    name: 'christmas5',
-    category: 'christmas',
-    image: <StickerImage src="/stickers/christmas/5.png" />,
-  },
-  {
-    name: 'christmas6',
-    category: 'christmas',
-    image: <StickerImage src="/stickers/christmas/6.png" />,
-  },
-  {
-    name: 'christmas7',
-    category: 'christmas',
-    image: <StickerImage src="/stickers/christmas/7.png" />,
-  },
-  {
-    name: 'christmas8',
-    category: 'christmas',
-    image: <StickerImage src="/stickers/christmas/8.png" />,
-  },
-  {
-    name: 'christmas9',
-    category: 'christmas',
-    image: <StickerImage src="/stickers/christmas/9.png" />,
-  },
-  {
-    name: 'christmas10',
-    category: 'christmas',
-    image: <StickerImage src="/stickers/christmas/10.png" />,
-  },
-  {
-    name: 'emoji1',
-    category: 'emoji',
-    image: <StickerImage src="/stickers/emoji/1.png" />,
-  },
-  {
-    name: 'emoji2',
-    category: 'emoji',
-    image: <StickerImage src="/stickers/emoji/2.png" />,
-  },
-  {
-    name: 'emoji3',
-    category: 'emoji',
-    image: <StickerImage src="/stickers/emoji/3.png" />,
-  },
-  {
-    name: 'emoji4',
-    category: 'emoji',
-    image: <StickerImage src="/stickers/emoji/4.png" />,
-  },
-  {
-    name: 'emoji5',
-    category: 'emoji',
-    image: <StickerImage src="/stickers/emoji/5.png" />,
-  },
-  {
-    name: 'emoji6',
-    category: 'emoji',
-    image: <StickerImage src="/stickers/emoji/6.png" />,
-  },
-  {
-    name: 'emoji7',
-    category: 'emoji',
-    image: <StickerImage src="/stickers/emoji/7.png" />,
-  },
-  {
-    name: 'emoji8',
-    category: 'emoji',
-    image: <StickerImage src="/stickers/emoji/8.png" />,
-  },
-  {
-    name: 'emoji9',
-    category: 'emoji',
-    image: <StickerImage src="/stickers/emoji/9.png" />,
-  },
-  {
-    name: 'emoji10',
-    category: 'emoji',
-    image: <StickerImage src="/stickers/emoji/10.png" />,
-  },
+  ...createStickers('animal', 10),
+  ...createStickers('christmas', 10),
+  ...createStickers('emoji', 10),
+  ...createStickers('flower', 10),
+  ...createStickers('fruit', 10),
+  ...createStickers('heart', 10),
+  ...createStickers('instrument', 10),
+  ...createStickers('member', 6),
+  ...createStickers('ocean', 10),
+  ...createStickers('pixel', 10),
+  ...createStickers('real_cat', 6),
 ];
