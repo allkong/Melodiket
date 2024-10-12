126f4d817fe672b2a36fb546382aeeb48a0d01b4
Dabin Jeong <allempti@gmail.com>
2024-10-12 19:46:37 +0900

    feat: 시작한 공연 예매 버튼 숨기기

--- frontend/melodiket/src/app/concerts/[uuid]/_components/concert-information.tsx
+++ frontend/melodiket/src/app/concerts/[uuid]/_components/concert-information.tsx
@@ -1,18 +1,18 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
 
-import { useFetchConcertDetail } from '@/services/concert/fetchConcert';
 import dayjs, { formatDateWithDayAndTime } from '@/utils/dayjsPlugin';
+import { useFetchConcertDetail } from '@/services/concert/fetchConcert';
+import useAuthStore from '@/store/authStore';
 
 import ThinDivider from '@/components/atoms/divider/ThinDivider';
 import ConcertTitle from './concert-title';
 import ConcertDetail from './concert-detail';
 import MusiciansInformation from './musician-information';
 import ConcertDescription from './concert-description';
-import LargeButton from '@/components/atoms/button/LargeButton';
-import useAuthStore from '@/store/authStore';
-import toast from 'react-hot-toast';
+import FixedButton from '@/components/organisms/controls/FixedButton';
 
 const getButtonMessage = (disabled: boolean, remainSeat: number) => {
   if (disabled) {
@@ -43,38 +43,43 @@ const ConcertInformation = ({ uuid }: ConcertInformationProps) => {
 
     if (concert?.isStanding) {
       router.push(`/concerts/${uuid}/book?step=confirm`);
+      return;
     } else {
       router.push(`/concerts/${uuid}/book?step=seat`);
+      return;
     }
   };
 
   const disabled = dayjs().isBefore(dayjs(concert?.ticketingAt));
+  const isBeforeConcert = dayjs().isBefore(dayjs(concert?.startAt));
 
   return (
-    <div className="relative w-full flex-grow h-fit px-7 pb-14 space-y-6">
-      <div className="absolute w-full h-10 -top-5 left-0 right-0 bg-white rounded-2xl" />
-      <ConcertTitle title={concert?.title} stageName={concert?.stageName} />
-      <ThinDivider />
-      <ConcertDetail
-        startAt={formatDateWithDayAndTime(concert?.startAt || '')}
-        ticketingAt={formatDateWithDayAndTime(concert?.ticketingAt || '')}
-        seatCapacity={concert?.capacity ?? 0}
-        ticketPrice={concert?.ticketPrice ?? 0}
-        isStanding={concert?.isStanding ?? false}
-        remainSeat={concert?.availableTickets ?? 0}
-      />
-      <ThinDivider />
-      <MusiciansInformation musicians={concert?.musicians} />
-      <ThinDivider />
-      <ConcertDescription description={concert?.description} />
-      <div className="fixed w-full max-w-xl bottom-0 left-1/2 -translate-x-1/2 px-6 py-3 bg-white">
-        <LargeButton
+    <>
+      <div className="relative w-full flex-grow h-fit px-7 pb-14 space-y-6">
+        <div className="absolute w-full h-10 -top-5 left-0 right-0 bg-white rounded-2xl" />
+        <ConcertTitle title={concert?.title} stageName={concert?.stageName} />
+        <ThinDivider />
+        <ConcertDetail
+          startAt={formatDateWithDayAndTime(concert?.startAt || '')}
+          ticketingAt={formatDateWithDayAndTime(concert?.ticketingAt || '')}
+          seatCapacity={concert?.capacity ?? 0}
+          ticketPrice={concert?.ticketPrice ?? 0}
+          isStanding={concert?.isStanding ?? false}
+          remainSeat={concert?.availableTickets ?? 0}
+        />
+        <ThinDivider />
+        <MusiciansInformation musicians={concert?.musicians} />
+        <ThinDivider />
+        <ConcertDescription description={concert?.description} />
+      </div>
+      {isBeforeConcert && (
+        <FixedButton
           label={getButtonMessage(disabled, concert?.availableTickets ?? 0)}
           onClick={handleClick}
           disabled={disabled || (concert?.availableTickets ?? 0) <= 0}
         />
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
