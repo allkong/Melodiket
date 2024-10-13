719251b0bd763014fdde1f7fef483c5e9af4ed4b
강혁준 <domohit5124@naver.com>
2024-10-13 23:19:55 +0900

    feat: MusicianProfileCard onClick props 추가

--- frontend/melodiket/src/app/_section/favorite-musician-section.tsx
+++ frontend/melodiket/src/app/_section/favorite-musician-section.tsx
@@ -2,38 +2,32 @@
 
 import MusicianProfileCard from '@/components/molecules/profile/MusicianProfileCard';
 import { useFetchFavoriteMusiciansList } from '@/services/favorite/fetchFavorite';
-import { getCidUrl, getS3Url } from '@/utils/getUrl';
+import { getS3Url } from '@/utils/getUrl';
 import NeedLogin from '../_components/need-login';
 import NoMusician from '../_components/no-musician';
 import useAuthStore from '@/store/authStore';
-import { FavoriteMusician } from '@/types/favorite';
-import { User } from '@/types/user';
-
-const getComponent = (user: User | null, data?: FavoriteMusician) => {
-  if (!user) {
-    return <NeedLogin />;
-  }
-
-  if (!data || data?.pageInfo.responsedSize === 0) {
-    return <NoMusician />;
-  }
-
-  return data.result.map((musician) => (
-    <MusicianProfileCard
-      key={musician.loginId}
-      musicianName={musician.nickname}
-      src={getS3Url(musician.imageUrl)}
-    />
-  ));
-};
+import { useRouter } from 'next/navigation';
 
 const FavoriteMusicianSection = () => {
   const { data } = useFetchFavoriteMusiciansList();
   const { user } = useAuthStore();
+  const router = useRouter();
 
-  const show = getComponent(user, data);
+  if (!user) return <NeedLogin />;
+  else if (!data || data?.pageInfo.responsedSize === 0) return <NoMusician />;
 
-  return <div className="flex gap-2">{show}</div>;
+  return (
+    <div className="flex gap-2">
+      {data.result.map((musician) => (
+        <MusicianProfileCard
+          key={musician.loginId}
+          musicianName={musician.nickname}
+          src={getS3Url(musician.imageUrl)}
+          onClick={() => router.push(`/musicians/${musician.uuid}`)}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default FavoriteMusicianSection;
